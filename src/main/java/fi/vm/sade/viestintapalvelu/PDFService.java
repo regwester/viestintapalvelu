915f2fa1abd08b0403b3457a487a3b1b787510ae
Iina Sipilä <iina.sipila@reaktor.fi>
2013-04-30 15:41:21 +0300

    Generating PDF with PDFBOx

--- src/main/java/fi/vm/sade/viestintapalvelu/PDFService.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/PDFService.java
@@ -1,36 +1,58 @@
 package fi.vm.sade.viestintapalvelu;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
 public class PDFService {
 	
-	public void generatePDF(List<String> students) {
+	public void createDocuments(List<String> students) {
 		for (String student : students) {
-			writeDocument(student, generatePDF(student));
+			createDocumentFor(student);
 		}
 	}
 	
-	private byte[] generatePDF(String name) {
-		return new byte[] {1,0};
-	}
-	
-	private void writeDocument(String name, byte[] document) {
+	private void createDocumentFor(String student) {
+		PDDocument doc = null;
+		OutputStream fos = null;
 		try {
-			FileOutputStream fos = new FileOutputStream("documents/"+name+".pdf", false);
-			fos.write(document, 0, document.length);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			doc = new PDDocument();
+			PDPage page = new PDPage();
+			doc.addPage(page);
+			PDFont font = PDType1Font.HELVETICA_BOLD;
+			PDPageContentStream stream = new PDPageContentStream(doc, page);
+			stream.beginText();
+			stream.setFont(font, 24);
+			stream.moveTextPositionByAmount(100, 700);
+			stream.drawString(student);
+			stream.endText();
+			stream.close();
+			fos = new FileOutputStream("documents/"+student+".pdf", false);
+			doc.save(fos);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (COSVisitorException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				doc.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
-		new PDFService().generatePDF(Arrays.asList(args));
+		new PDFService().createDocuments(Arrays.asList(args));
 	}
 
 }
