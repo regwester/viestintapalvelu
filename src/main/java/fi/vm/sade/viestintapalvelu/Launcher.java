ca4d4af276ae298242a3ee9395dd4b391f97d820
Ville Peurala <ville.peurala@gmail.com>
2013-04-30 21:32:47 +0300

    Cleanup

--- src/main/java/fi/vm/sade/viestintapalvelu/Launcher.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/Launcher.java
@@ -3,26 +3,24 @@ package fi.vm.sade.viestintapalvelu;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 
-import com.sun.jersey.api.core.ApplicationAdapter;
-import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class Launcher {
+	private static final String SERVLET_NAME = "viestintapalvelu";
+	private static final int DEFAULT_PORT = 8080;
+
 	public static void main(String[] args) throws Exception {
 		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8080);
+		tomcat.setPort(DEFAULT_PORT);
 
 		Context ctx = tomcat.addContext("/",
 				System.getProperty("java.io.tmpdir"));
 
 		ViestintapalveluApplication application = new ViestintapalveluApplication();
 
-		ApplicationAdapter adapter = new ApplicationAdapter(application);
-		adapter.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, true);
-
 		ServletContainer container = new ServletContainer(application);
-		Tomcat.addServlet(ctx, "viestintapalvelu", container);
-		ctx.addServletMapping("/*", "viestintapalvelu");
+		Tomcat.addServlet(ctx, SERVLET_NAME, container);
+		ctx.addServletMapping("/*", SERVLET_NAME);
 
 		tomcat.start();
 		tomcat.getServer().await();
