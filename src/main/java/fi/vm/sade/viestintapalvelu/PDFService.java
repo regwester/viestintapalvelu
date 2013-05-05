500ba31eb142a125dd23619bb474ca7ed02d6a06
Iina Sipilä <iina.sipila@reaktor.fi>
2013-05-05 23:48:52 +0300

    Fixed xerces dependency problem

--- src/main/java/fi/vm/sade/viestintapalvelu/PDFService.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/PDFService.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 import fr.opensagres.xdocreport.converter.ConverterTypeTo;
@@ -16,26 +17,8 @@ import fr.opensagres.xdocreport.document.IXDocReport;
 import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
 import fr.opensagres.xdocreport.template.IContext;
 import fr.opensagres.xdocreport.template.TemplateEngineKind;
-import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 
 public class PDFService {
-	public byte[] printAddressLabels(AddressLabelBatch input) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try {
-			IXDocReport report = readTemplate(input.getTemplateName());
-			Options options = Options.getTo(ConverterTypeTo.PDF).via(
-					ConverterTypeVia.ODFDOM);
-			report.convert(createDataContext(input.getAddressLabels(), report),
-					options, out);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (XDocReportException e) {
-			e.printStackTrace();
-		}
-		return out.toByteArray();
-	}
 
 	public void createDocuments(List<String> hakijat) {
 		for (String hakija : hakijat) {
@@ -67,21 +50,6 @@ public class PDFService {
 		return context;
 	}
 
-	private IContext createDataContext(List<AddressLabel> labels,
-			IXDocReport report) throws XDocReportException {
-		IContext context = report.createContext();
-		context.put("labels", labels);
-		FieldsMetadata metadata = new FieldsMetadata();
-		metadata.addFieldAsList("labels.FirstName");
-		metadata.addFieldAsList("labels.LastName");
-		metadata.addFieldAsList("labels.StreetAddress");
-		metadata.addFieldAsList("labels.PostalCode");
-		metadata.addFieldAsList("labels.PostOffice");
-		metadata.addFieldAsList("labels.Country");
-		report.setFieldsMetadata(metadata);
-		return context;
-	}
-
 	private IXDocReport readTemplate(String templateName)
 			throws FileNotFoundException, IOException, XDocReportException {
 		InputStream in = getClass().getResourceAsStream(templateName);
@@ -110,20 +78,8 @@ public class PDFService {
 		}
 	}
 
-	// public static void main(String[] args) throws Exception {
-	// AddressLabelBatch batch = new AddressLabelBatch();
-	// List<AddressLabel> labels = new ArrayList<AddressLabel>();
-	// labels.add(new AddressLabel("Meiju", "", "Aalto-Setala",
-	// "Kauriintie 3 D 31", "00740", "HELSINKI", "Suomi"));
-	// labels.add(new AddressLabel("Aarnio", "", "Lumi", "Tilkankatu 7b 24",
-	// "00300", "HELSINKI", "Suomi"));
-	// labels.add(new AddressLabel("Maria", "", "Abbas-Hashimi",
-	// "Petter wetterin tie 6 c 46", "00810", "Helsinki", "Suomi"));
-	// labels.add(new AddressLabel("Hodan", "", "Abdi", "Postirestante",
-	// "00840", "Helsinki", "Suomi"));
-	// batch.setAddressLabels(labels);
-	// batch.setTemplateName("/osoitetarrat.odt");
-	// PDFService service = new PDFService();
-	// service.writeDocument("tarrat", service.printAddressLabels(batch));
-	// }
+	 public static void main(String[] args) throws Exception {
+		 PDFService service = new PDFService();
+		 service.writeDocument("tarrat", service.createDocument("Iina"));
+	 }
 }
