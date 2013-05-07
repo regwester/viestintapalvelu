e9be65b385799ab07a4da522004fe1ee4cd09ae1
Ville Peurala <ville.peurala@reaktor.fi>
2013-05-07 13:38:44 +0300

    Use a global Tomcat instance

--- src/test/java/fi/vm/sade/viestintapalvelu/TomcatRule.java
+++ src/test/java/fi/vm/sade/viestintapalvelu/TomcatRule.java
@@ -1,25 +1,20 @@
 package fi.vm.sade.viestintapalvelu;
 
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.junit.rules.ExternalResource;
 
 public class TomcatRule extends ExternalResource {
-	private Tomcat tomcat;
+	private static Tomcat globalTomcat;
 
 	@Override
 	protected void before() throws Exception {
-		tomcat = Launcher.start();
+		if (globalTomcat == null) {
+			globalTomcat = Launcher.start();
+		}
 	}
 
 	@Override
 	protected void after() {
-		if (tomcat != null) {
-			try {
-				tomcat.destroy();
-			} catch (LifecycleException e) {
-				throw new RuntimeException(e);
-			}
-		}
+		// Do nothing, let Tomcat run until the JVM dies.
 	}
 }
