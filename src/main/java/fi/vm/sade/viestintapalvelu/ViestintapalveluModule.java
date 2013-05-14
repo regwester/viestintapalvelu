5530d6380f20175f5f1d59ae3ddfbea25560bda6
Ville Peurala <ville.peurala@reaktor.fi>
2013-05-14 13:36:28 +0300

    mvn tomcat7:run works now

--- src/main/java/fi/vm/sade/viestintapalvelu/ViestintapalveluModule.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/ViestintapalveluModule.java
@@ -3,6 +3,7 @@ package fi.vm.sade.viestintapalvelu;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -17,7 +18,9 @@ public class ViestintapalveluModule extends JerseyServletModule {
 
 		Map<String, String> initParameters = new HashMap<String, String>();
 		initParameters.put(JSONConfiguration.FEATURE_POJO_MAPPING, "true");
+		initParameters.put(ResourceConfig.FEATURE_TRACE, "true");
+		initParameters.put(ResourceConfig.FEATURE_TRACE_PER_REQUEST, "true");
 
-		serve("/*").with(GuiceContainer.class, initParameters);
+		serve("/api/v1/*").with(GuiceContainer.class, initParameters);
 	}
 }
