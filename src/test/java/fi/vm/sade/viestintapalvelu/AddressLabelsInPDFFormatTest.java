6ca63043cacc6e9fe274ee69f90ce9860482e3ca
Iina Sipilä <iina.sipila@reaktor.fi>
2013-05-21 13:38:02 +0300

    use testutil

--- src/test/java/fi/vm/sade/viestintapalvelu/AddressLabelsInPDFFormatTest.java
+++ src/test/java/fi/vm/sade/viestintapalvelu/AddressLabelsInPDFFormatTest.java
@@ -1,44 +1,19 @@
 package fi.vm.sade.viestintapalvelu;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFText2HTML;
-import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-import org.w3c.tidy.Configuration;
-import org.w3c.tidy.Tidy;
 
 import fi.vm.sade.viestintapalvelu.address.AddressLabel;
-import fi.vm.sade.viestintapalvelu.address.AddressLabelBatch;
 import fi.vm.sade.viestintapalvelu.testdata.Generator;
 
 @RunWith(Enclosed.class)
@@ -289,51 +264,6 @@ public class AddressLabelsInPDFFormatTest {
 		}
 	}
 
-	private static List<String[]> readDownloadResponseBody(HttpResponse response)
-			throws IOException, DocumentException {
-		PDDocument document = PDDocument
-				.load(response.getEntity().getContent());
-		PDFText2HTML stripper = new PDFText2HTML("UTF-8");
-		StringWriter writer = new StringWriter();
-		stripper.setLineSeparator("<br/>");
-		stripper.writeText(document, writer);
-		document.close();
-		return parseHTML(new String(toXhtml(writer.toString().getBytes())));
-	}
-
-	private static String readCreateDocumentResponseBody(HttpResponse response)
-			throws IOException, DocumentException {
-		return IOUtils.toString(response.getEntity().getContent(), "UTF-8");
-	}
-
-	private static byte[] toXhtml(byte[] document) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		newTidy().parseDOM(new ByteArrayInputStream(document), out);
-		return out.toByteArray();
-	}
-
-	private static Tidy newTidy() {
-		Tidy tidy = new Tidy();
-		tidy.setTidyMark(false);
-		tidy.setDocType("omit");
-		tidy.setXHTML(true);
-		tidy.setCharEncoding(Configuration.UTF8);
-		tidy.setQuiet(true);
-		return tidy;
-	}
-
-	private static List<String[]> parseHTML(String xml)
-			throws DocumentException {
-		SAXReader reader = new SAXReader();
-		List<String[]> labels = new ArrayList<String[]>();
-		Document document = reader.read(new StringReader(xml));
-		for (Object object : document.selectNodes("//div/p")) {
-			Node p = (Node) object;
-			labels.add(p.getText().split("\n"));
-		}
-		return labels;
-	}
-
 	private static List<AddressLabel> createLabels(int count)
 			throws JsonParseException, JsonMappingException, IOException {
 		return new Generator<AddressLabel>() {
@@ -358,29 +288,4 @@ public class AddressLabelsInPDFFormatTest {
 						addressline, addressline2, addressline3, postalCode,
 						postOffice, region, country))).get(0);
 	}
-
-	private final static String PDF_TEMPLATE = "/osoitetarrat.html";
-
-	private static List<String[]> callGenerateLabels(List<AddressLabel> labels)
-			throws UnsupportedEncodingException, IOException,
-			JsonGenerationException, JsonMappingException,
-			ClientProtocolException, DocumentException {
-		AddressLabelBatch batch = new AddressLabelBatch(PDF_TEMPLATE, labels);
-		DefaultHttpClient client = new DefaultHttpClient();
-		client.getParams().setParameter("http.protocol.content-charset",
-				"UTF-8");
-		HttpPost post = new HttpPost("http://localhost:"
-				+ Launcher.DEFAULT_PORT + "/" + Urls.API_PATH + "/"
-				+ Urls.ADDRESS_LABEL_RESOURCE_PATH + "/pdf");
-		post.setHeader("Content-Type", "application/json;charset=utf-8");
-		post.setEntity(new StringEntity(new ObjectMapper()
-				.writeValueAsString(batch), ContentType.APPLICATION_JSON));
-		HttpResponse response = client.execute(post);
-		String documentId = readCreateDocumentResponseBody(response);
-		HttpGet get = new HttpGet(
-				"http://localhost:8080/api/v1/download/document/" + documentId);
-		response = client.execute(get);
-		return readDownloadResponseBody(response);
-	}
-
 }
