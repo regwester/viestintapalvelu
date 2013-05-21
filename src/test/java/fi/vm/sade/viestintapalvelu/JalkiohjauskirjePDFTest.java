fd54da05157e1f782c582c63671ac092d66b1311
Iina Sipilä <iina.sipila@reaktor.fi>
2013-05-21 14:41:56 +0300

    hyvaksymiskirje testit

--- src/test/java/fi/vm/sade/viestintapalvelu/JalkiohjauskirjePDFTest.java
+++ src/test/java/fi/vm/sade/viestintapalvelu/JalkiohjauskirjePDFTest.java
@@ -1,16 +1,12 @@
 package fi.vm.sade.viestintapalvelu;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 
 import fi.vm.sade.viestintapalvelu.address.AddressLabel;
 import fi.vm.sade.viestintapalvelu.jalkiohjauskirje.Jalkiohjauskirje;
@@ -21,32 +17,22 @@ public class JalkiohjauskirjePDFTest {
 
 	private static AddressLabel label = new AddressLabel("Åle", "Öistämö",
 			"Brännkyrksgatan 177 B 149", "Södermalm", "13", "65330", "Stockholm", "SL", "Sweden");
-	private static List<String> pdfLabel;
+	private static String pdf;
 
 	@BeforeClass
 	public static void setUp() throws Exception {
 		Jalkiohjauskirje kirje = new Jalkiohjauskirje(label, new ArrayList<Map<String,String>>());
-		List<List<String>> pdf = TestUtil.generateJalkiohjauskirje(kirje);
-		pdfLabel = findAddressLabel(pdf);
+		pdf = TestUtil.generateJalkiohjauskirje(kirje).toString();
 	}
 
 	@Test
 	public void isPrintedWithAddressLabel() throws Exception {
-		Assert.assertEquals(label.getFirstName() + " " + label.getLastName(), pdfLabel.get(0));
-		Assert.assertEquals(label.getAddressline(), pdfLabel.get(1));
-		Assert.assertEquals(label.getAddressline2(), pdfLabel.get(2));
-		Assert.assertEquals(label.getAddressline3(), pdfLabel.get(3));
-		Assert.assertEquals(label.getPostalCode() + " " + label.getCity(), pdfLabel.get(4));
-		Assert.assertEquals(label.getRegion(), pdfLabel.get(5));
-		Assert.assertEquals(label.getCountry(), pdfLabel.get(6));
-	}
-
-	private static List<String> findAddressLabel(List<List<String>> pdf) {
-		List<String> address = Iterables.find(pdf, new Predicate<List<String>>() {
-			public boolean apply(List<String> element) {
-				return element.size() > 0 && element.get(0).startsWith("Priority PP Finlande");
-			}
-		});
-		return address.subList(1, address.size());
+		Assert.assertTrue("Firstname Lastname missing", pdf.contains(label.getFirstName() + " " + label.getLastName()));
+		Assert.assertTrue("Addressline missing", pdf.contains(label.getAddressline()));
+		Assert.assertTrue("Addressline2 missing", pdf.contains(label.getAddressline2()));
+		Assert.assertTrue("Addressline3 missing", pdf.contains(label.getAddressline3()));
+		Assert.assertTrue("Postalcode city missing", pdf.contains(label.getPostalCode() + " " + label.getCity()));
+		Assert.assertTrue("Region missing", pdf.contains(label.getRegion()));
+		Assert.assertTrue("Country missing", pdf.contains(label.getCountry()));
 	}
 }
--- src/test/java/fi/vm/sade/viestintapalvelu/TestUtil.java
+++ src/test/java/fi/vm/sade/viestintapalvelu/TestUtil.java
@@ -36,6 +36,8 @@ import org.w3c.tidy.Tidy;
 
 import fi.vm.sade.viestintapalvelu.address.AddressLabel;
 import fi.vm.sade.viestintapalvelu.address.AddressLabelBatch;
+import fi.vm.sade.viestintapalvelu.hyvaksymiskirje.Hyvaksymiskirje;
+import fi.vm.sade.viestintapalvelu.hyvaksymiskirje.HyvaksymiskirjeBatch;
 import fi.vm.sade.viestintapalvelu.jalkiohjauskirje.Jalkiohjauskirje;
 import fi.vm.sade.viestintapalvelu.jalkiohjauskirje.JalkiohjauskirjeBatch;
 
@@ -44,9 +46,11 @@ public class TestUtil {
 	private final static String ADDRESS_LABEL_PDF_URL = "http://localhost:8080/api/v1/addresslabel/pdf";
 	private final static String ADDRESS_LABEL_XLS_URL = "http://localhost:8080/api/v1/addresslabel/xls";
 	private final static String JALKIOHJAUSKIRJE_URL = "http://localhost:8080/api/v1/jalkiohjauskirje/pdf";
+	private final static String HYVAKSYMISKIRJE_URL = "http://localhost:8080/api/v1/hyvaksymiskirje/pdf";
 	private final static String ADDRESS_LABEL_PDF_TEMPLATE = "/osoitetarrat.html";
 	private final static String ADDRESS_LABEL_XLS_TEMPLATE = "/osoitetarrat.xls";
 	private final static String JALKIOHJAUSKIRJE_TEMPLATE = "/jalkiohjauskirje.html";
+	private final static String HYVAKSYMISKIRJE_TEMPLATE = "/hyvaksymiskirje.html";
 	private final static String LIITE_TEMPLATE = "/liite.html";
 	private final static String HAKUTULOSTAULUKKO_TEMPLATE = "/hakutulostaulukko_test.html";
 	
@@ -63,6 +67,11 @@ public class TestUtil {
 	public static List<List<String>> generateJalkiohjauskirje(Jalkiohjauskirje kirje) throws Exception {
 		JalkiohjauskirjeBatch batch = new JalkiohjauskirjeBatch(JALKIOHJAUSKIRJE_TEMPLATE, LIITE_TEMPLATE, Arrays.asList(kirje));
 		return readPDF(get(batch, JALKIOHJAUSKIRJE_URL), 1, 2);
+	}
+	
+	public static List<List<String>> generateHyvaksymiskirje(Hyvaksymiskirje kirje) throws Exception {
+		HyvaksymiskirjeBatch batch = new HyvaksymiskirjeBatch(HYVAKSYMISKIRJE_TEMPLATE, LIITE_TEMPLATE, Arrays.asList(kirje));
+		return readPDF(get(batch, HYVAKSYMISKIRJE_URL), 1, 2);
 	}
 	
 	public static List<List<String>> generateHakutulostaulukko(Jalkiohjauskirje kirje) throws Exception {
