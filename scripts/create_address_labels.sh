6e73f14d1ad6916ca10cb4f9173e989851d4371a
Ville Peurala <ville.peurala@reaktor.fi>
2013-05-27 13:31:42 +0300

    Delete unused shell scripts

--- scripts/create_address_labels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure that target dir exists
-mkdir ../target
-
-# Download POST response to ${PROJECT_ROOT}/target/addresslabels.pdf
-curl -i -H "Content-Type: application/json" -X POST -d @../src/test/resources/addresslabelbatch1.json http://localhost:8080/api/v1/addresslabel > ../target/addresslabels.pdf
-
--- scripts/create_address_labels_csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Ensure that target dir exists
-mkdir ../target
-
-# Download POST response to ${PROJECT_ROOT}/target/addresslabels.csv
-curl -i -H "Content-Type: application/json" -X POST -d @../src/test/resources/addresslabel_csv.json http://localhost:8080/api/v1/addresslabel > ../target/addresslabels.csv
-
