2988fc884b85146afe48a6f4786595836d618b7d
Ville Peurala <ville.peurala@reaktor.fi>
2013-05-29 14:47:03 +0300

    Fix scandic chars on some computers.

--- src/main/java/fi/vm/sade/viestintapalvelu/Constants.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/Constants.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.viestintapalvelu;
 
+import java.nio.charset.Charset;
+
 // TODO vpeurala 22.5.2013: Maybe make these configurable?
 public class Constants {
 	public static final int IPOST_BATCH_LIMIT = 10;
@@ -7,4 +9,5 @@ public class Constants {
 	public final static String LIITE_TEMPLATE = "/liite_{LANG}.html";
 	public final static String JALKIOHJAUSKIRJE_TEMPLATE = "/jalkiohjauskirje_{LANG}.html";
 	public final static String IPOST_TEMPLATE = "/ipost.xml";
+	public final static Charset UTF_8 = Charset.forName("UTF-8");
 }
