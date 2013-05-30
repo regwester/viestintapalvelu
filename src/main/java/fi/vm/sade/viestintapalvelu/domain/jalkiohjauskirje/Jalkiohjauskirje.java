db657eee6600d6f9c081eeeb2517e64f3dee4c40
Ville Peurala <ville.peurala@reaktor.fi>
2013-05-30 17:02:10 +0300

    Jalkiohjauskirje from class to interface

--- src/main/java/fi/vm/sade/viestintapalvelu/domain/jalkiohjauskirje/Jalkiohjauskirje.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/domain/jalkiohjauskirje/Jalkiohjauskirje.java
@@ -4,50 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import fi.vm.sade.viestintapalvelu.domain.address.HasPostalAddress;
-import fi.vm.sade.viestintapalvelu.domain.address.PostalAddress;
 
-public class Jalkiohjauskirje implements HasPostalAddress {
-	public Jalkiohjauskirje() {
-	}
-
-	public Jalkiohjauskirje(PostalAddress postalAddress, String languageCode,
-			List<Map<String, String>> tulokset) {
-		this.postalAddress = postalAddress;
-		this.languageCode = languageCode;
-		this.tulokset = tulokset;
-	}
-
-	/**
-	 * Osoitetiedot.
-	 */
-	private PostalAddress postalAddress;
+public interface Jalkiohjauskirje extends HasPostalAddress {
 	/**
 	 * Kielikoodi ISO 639-1.
 	 */
-	private String languageCode;
-	/**
-	 * Hakutulokset.
-	 */
-	private List<Map<String, String>> tulokset;
+	public String getLanguageCode();
 
-	@Override
-	public PostalAddress postalAddress() {
-		return postalAddress;
-	}
-
-	public String getLanguageCode() {
-		return languageCode;
-	}
-
-	public List<Map<String, String>> getTulokset() {
-		return tulokset;
-	}
-
-	@Override
-	public String toString() {
-		return "Jalkiohjauskirje [postalAddress=" + postalAddress
-				+ ", languageCode=" + languageCode + ", results=" + tulokset
-				+ "]";
-	}
-
+	public List<Map<String, String>> getTulokset();
 }
