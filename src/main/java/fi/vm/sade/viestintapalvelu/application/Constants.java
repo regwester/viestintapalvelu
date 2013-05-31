6c3e5926deb0feb6c914b40d202ae08e2e02f6d9
Ville Peurala <ville.peurala@gmail.com>
2013-05-31 21:00:28 +0300

    Inline UTF-8 charset

--- src/main/java/fi/vm/sade/viestintapalvelu/application/Constants.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/application/Constants.java
@@ -1,7 +1,5 @@
 package fi.vm.sade.viestintapalvelu.application;
 
-import java.nio.charset.Charset;
-
 // TODO vpeurala 22.5.2013: Maybe make these configurable?
 public class Constants {
 	public static final int IPOST_BATCH_LIMIT = 10;
@@ -9,5 +7,4 @@ public class Constants {
 	public final static String LIITE_TEMPLATE = "/liite_{LANG}.html";
 	public final static String JALKIOHJAUSKIRJE_TEMPLATE = "/jalkiohjauskirje_{LANG}.html";
 	public final static String IPOST_TEMPLATE = "/ipost.xml";
-	public final static Charset UTF_8 = Charset.forName("UTF-8");
 }
