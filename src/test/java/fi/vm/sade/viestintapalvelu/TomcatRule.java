ba92db63ce0013e2b852d248ea384353f7dd264b
Ville Peurala <ville.peurala@gmail.com>
2013-06-03 22:53:29 +0300

    Check port connectivity instead of global Tomcat instance, which does not work if there are multiple JVMs running in parallel

--- src/test/java/fi/vm/sade/viestintapalvelu/TomcatRule.java
+++ src/test/java/fi/vm/sade/viestintapalvelu/TomcatRule.java
@@ -1,11 +1,11 @@
 package fi.vm.sade.viestintapalvelu;
 
 import java.io.IOException;
+import java.net.Socket;
 
 import javax.servlet.ServletException;
 
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -14,19 +14,21 @@ import org.junit.rules.ExternalResource;
 import fi.vm.sade.viestintapalvelu.test.Localhost;
 
 public class TomcatRule extends ExternalResource {
-	private static Tomcat globalTomcat;
-
 	@Override
 	protected void before() throws Exception {
-		if (globalTomcat == null) {
-			launchAndInitialize();
+		if (!isPortInUse()) {
+			synchronized (TomcatRule.class) {
+				if (!isPortInUse()) {
+					launchAndInitialize();
+				}
+			}
 		}
 	}
 
 	private void launchAndInitialize() throws LifecycleException,
 			ServletException, IOException, ClientProtocolException {
 		try {
-			globalTomcat = Launcher.start();
+			Launcher.start();
 			// Do one REST api call to initialize Jersey & Guice
 			// before running other tests
 			DefaultHttpClient client = new DefaultHttpClient();
@@ -49,5 +51,14 @@ public class TomcatRule extends ExternalResource {
 	@Override
 	protected void after() {
 		// Do nothing, let Tomcat run until the JVM dies.
+	}
+
+	private static boolean isPortInUse() {
+		try {
+			new Socket("localhost", Launcher.DEFAULT_PORT);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
 	}
 }
