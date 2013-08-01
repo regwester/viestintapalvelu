503ec1df355526fccd47c1430bc0aa12b2fed0d0
jussijartamo <jussi@jartamo@gofore.com>
2013-08-01 12:31:43 +0300

    VT-384 Allow cross domain

--- src/main/java/fi/vm/sade/viestintapalvelu/download/DownloadResource.java
+++ src/main/java/fi/vm/sade/viestintapalvelu/download/DownloadResource.java
@@ -1,9 +1,5 @@
 package fi.vm.sade.viestintapalvelu.download;
 
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-import fi.vm.sade.viestintapalvelu.Urls;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,6 +7,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import fi.vm.sade.viestintapalvelu.Urls;
 
 @Singleton
 @Path(Urls.DOWNLOAD_RESOURCE_PATH)
@@ -24,18 +25,15 @@ public class DownloadResource {
 
     @GET
     @Path("{documentId}")
-    public Response download(@PathParam("documentId") String input,
-                             @Context HttpServletResponse response) {
+    public Response download(@PathParam("documentId") String input, @Context HttpServletResponse response) {
         Download download = downloadCache.get(input);
         if (download == null) {
             return Response.status(Status.BAD_REQUEST).build();
         }
         response.setHeader("Content-Type", download.getContentType());
-        response.setHeader("Content-Disposition", "attachment; filename=\""
-                + download.getFilename() + "\"");
-        response.setHeader("Content-Length",
-                String.valueOf(download.toByteArray().length));
-        return Response.ok(download.toByteArray())
-                .type(download.getContentType()).build();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + download.getFilename() + "\"");
+        response.setHeader("Content-Length", String.valueOf(download.toByteArray().length));
+        return Response.ok(download.toByteArray()).type(download.getContentType()).build();
     }
 }
