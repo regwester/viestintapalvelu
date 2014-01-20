718ac8af880e74aa8d8947a9bd04968cc5f6d952
Juha Korkalainen <juha.korkalainen@iki.fi>
2014-01-20 11:24:00 +0200

    Update email.js

--- ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/app/js/email.js
+++ ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/app/js/email.js
@@ -86,6 +86,28 @@ email.controller('EmailController', ['$scope', '$rootScope', 'EmailAttachmentFac
 			$rootScope.callingProcess = $scope.emaildata.headers[0].callingProcess;			
 		};
 		
-	
+			$scope.files = [];
+	    $scope.percentage = 0;
+
+	    $scope.upload = function () {
+	    	uploadManager.upload();
+	        $scope.files = [];
+	    };
+
+	    $rootScope.$on('fileAdded', function (e, call) {
+	        $scope.files.push(call);
+	        $scope.$apply();
+	    });
+
+	    $rootScope.$on('uploadProgress', function (e, call) {
+	        $scope.percentage = call;
+	        $scope.$apply();
+	    });
+	    
+	    $rootScope.$on('uploadDone', function (e, call) {
+	        alert(call);
+	    	$scope.$apply();
+	    });
+	    
 }]);
 
