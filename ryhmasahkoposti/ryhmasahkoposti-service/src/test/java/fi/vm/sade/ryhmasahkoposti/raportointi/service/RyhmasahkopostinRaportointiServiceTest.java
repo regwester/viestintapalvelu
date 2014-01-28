46c8e37d3eebd618979f8041f5793f3a9a2dac59
unknown <veli-matti.heikkinen@cybercom.com>
2014-01-28 11:33:14 +0200

    Korjattu metodikutsun parametrit.

--- ryhmasahkoposti/ryhmasahkoposti-service/src/test/java/fi/vm/sade/ryhmasahkoposti/raportointi/service/RyhmasahkopostinRaportointiServiceTest.java
+++ ryhmasahkoposti/ryhmasahkoposti-service/src/test/java/fi/vm/sade/ryhmasahkoposti/raportointi/service/RyhmasahkopostinRaportointiServiceTest.java
@@ -111,7 +111,7 @@ public class RyhmasahkopostinRaportointiServiceTest {
 
 		Long viestiID = ryhmasahkopostinRaportointiService.raportoiLahetyksenAloitus(lahetyksenAloitus);		
 
-		RaportoitavaViesti raportoitavaViesti = raportoitavaViestiService.haeRaportoitavaViesti(viestiID, false);
+		RaportoitavaViesti raportoitavaViesti = raportoitavaViestiService.haeRaportoitavaViesti(viestiID);
 		
 		assertNotNull(raportoitavaViesti);
 		assertNotNull(raportoitavaViesti.getId());
@@ -193,7 +193,7 @@ public class RyhmasahkopostinRaportointiServiceTest {
 		
 		assertTrue(vastaus);
 		
-		RaportoitavaViesti raportoitavaViesti = raportoitavaViestiService.haeRaportoitavaViesti(viestiID, false);
+		RaportoitavaViesti raportoitavaViesti = raportoitavaViestiService.haeRaportoitavaViesti(viestiID);
 		
 		assertNotNull(raportoitavaViesti);
 		assertNotNull(raportoitavaViesti.getRaportoitavatVastaanottajat());
