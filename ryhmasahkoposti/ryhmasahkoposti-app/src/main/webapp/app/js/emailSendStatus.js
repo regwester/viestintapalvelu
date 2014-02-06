ca5dca0b86d2b775214ebd885923666f2b16cd06
gardziella <mikael.gardziella@cybercom.com>
2014-02-06 08:53:09 +0200

    Statuspage percentage bar.

--- ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/app/js/emailSendStatus.js
+++ ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/app/js/emailSendStatus.js
@@ -4,7 +4,6 @@ var emailStatus = angular.module('viestintapalvelu');
 emailStatus.controller('EmailSendStatusController', ['$scope', '$rootScope', 'EmailSendStatusFactory', '$location', '$timeout', 
                                                      function($scope, $rootScope, EmailSendStatusFactory, $location, $timeout) { 	
 
-	$scope.percentage  = 0;		
 	$scope.emailsendid = $rootScope.emailsendid;
 
 	$scope.getStatus = function () {
@@ -12,29 +11,27 @@ emailStatus.controller('EmailSendStatusController', ['$scope', '$rootScope', 'Em
 		
 		$scope.LahetyksenTilanneDTO = EmailSendStatusFactory.sendEmailStatus($scope.emailsendid.id);	
 	
-		var ok = $scope.LahetyksenTilanneDTO.lahetysOnnistuiLukumaara;
-		var notOk = $scope.LahetyksenTilanneDTO.lahetysEpaonnistuiLukumaara;
-		var all = $scope.LahetyksenTilanneDTO.vastaanottajienLukumaara;
-		
-		$scope.percentage = parseInt((ok + notOk) / all * 100, 10);    		
-		
-		if (all == ok + notOk) {
-			$location.path("/response");        			
+		$scope.percentage = function (ok, notOk, all) {
+			var o = parseInt(ok);
+			var nO =  parseInt(notOk)
+			var a = parseInt(all);
 			
-			$timeout.cancel($rootScope.promise);	
+			if (all == ok + notOk) {
+				$location.path("/response");        						
+				$timeout.cancel($rootScope.promise);				
+			}			
+			return ((ok + notOk +1) / (all +1)) * 100;			
 		}
-
-        $rootScope.promise = $timeout($scope.getStatus, 3000);	
+		
+	        $rootScope.promise = $timeout($scope.getStatus, 3000);				
 	}	
 	
-	$scope.updateStatus = function () {
-		$location.path("/status");        		
-	};
-	
+//	$scope.updateStatus = function () {
+//		$location.path("/status");        		
+//	};	
 	
 	$scope.statisticsEmail = function () {
-		$timeout.cancel($rootScope.promise);	
-		
+		$timeout.cancel($rootScope.promise);			
 		$location.path("/response");        		
 	};
 	
@@ -43,14 +40,14 @@ emailStatus.controller('EmailSendStatusController', ['$scope', '$rootScope', 'Em
 	// Start the poll	
 //	$scope.getStatus();
 		
-//	// Stop the poll
-//	$scope.$on('$locationChangeStart', function() {
-//	    $timeout.cancel($rootScope.promise);
-//	});	
-//	
-//	// Stop the poll 2
-//	$scope.$on('$destroy', function() {
-//	    $timeout.cancel($rootScope.promise);
-//	});
+	// Stop the poll
+	$scope.$on('$locationChangeStart', function() {
+	    $timeout.cancel($rootScope.promise);
+	});	
+	
+	// Stop the poll 2
+	$scope.$on('$destroy', function() {
+	    $timeout.cancel($rootScope.promise);
+	});
 	
 }]);
