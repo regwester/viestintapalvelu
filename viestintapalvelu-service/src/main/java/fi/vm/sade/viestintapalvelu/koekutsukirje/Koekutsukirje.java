199c7b532935088aa408da397671a9c5fdea93a7
jussijartamo <jussi@jartamo@gofore.com>
2014-03-06 13:59:01 +0200

    koekutsukirjeisiin valinnainen tarjoajanimi (opetuspiste)

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/koekutsukirje/Koekutsukirje.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/koekutsukirje/Koekutsukirje.java
@@ -12,68 +12,73 @@ import fi.vm.sade.viestintapalvelu.address.AddressLabel;
 
 @ApiModel(value = "Koekutsukirjemallipohjaan sisällytettävät kirjekohtaiset tiedot")
 public class Koekutsukirje {
-    public Koekutsukirje() {
-    }
+	public Koekutsukirje() {
+	}
 
-    public Koekutsukirje(AddressLabel addressLabel, String languageCode,
-                           String hakukohde, String letterBodyText, List<Map<String, String>> customLetterContents) {
-        this.addressLabel = addressLabel;
-        this.languageCode = StringUtils.isEmpty(languageCode) ? "FI" : languageCode;
-        this.hakukohde = hakukohde;
-        this.letterBodyText = letterBodyText;
-        this.customLetterContents = customLetterContents;
-    }
-    
-    @ApiModelProperty(value = "Osoitetiedot", required=true)
-    private AddressLabel addressLabel;
+	public Koekutsukirje(AddressLabel addressLabel, String languageCode,
+			String hakukohde, String letterBodyText,
+			List<Map<String, String>> customLetterContents) {
+		this.addressLabel = addressLabel;
+		this.languageCode = StringUtils.isEmpty(languageCode) ? "FI"
+				: languageCode;
+		this.hakukohde = hakukohde;
+		this.letterBodyText = letterBodyText;
+		this.customLetterContents = customLetterContents;
+	}
 
-    @ApiModelProperty(value = "Kielikoodi ISO 639-1, default = 'FI'")
-    private String languageCode;
+	@ApiModelProperty(value = "Osoitetiedot", required = true)
+	private AddressLabel addressLabel;
 
-    @ApiModelProperty(value = "Koekutsukirjeen leipäteksti, käyttäjältä HTML-muodossa, ajetaan palvelussa JSoup.clean-filtterin läpi", required=true, notes="Tämä kenttä on turhaan jokaisen kirjeen osa. Olisi hyvä siirtää osaksi luokkaa <code>KoekutsukirjeBatch<code>")
-    private String letterBodyText;
-    
-    @ApiModelProperty(value = "Valinnoista tuleva tieto siitä mihin (kouluun, koulutusohjelmaan jne. ) kokeella haetaan.", required=true)
-    private String hakukohde; 
+	@ApiModelProperty(value = "Kielikoodi ISO 639-1, default = 'FI'")
+	private String languageCode;
 
-    @ApiModelProperty(value = "Reserved for future", required=false, notes = 
-    		"Placeholder toistaiseksi. Voidaan välittää avain-arvo-pareina mahdollisia lisätietoja, " +
-            "joita voidaan käyttää mahdollisesti jatkossa esimerkiksi _hakukohteen_ tai " +  
-            "_kokeen_ tietojen täyttämiseksi koekutsukirjeeseen. Tässä vaiheessa (20140124, versio 8?), kun virkailijat saavat " +
-            "kirjoittaa koko kirjeen editorilla, ei ole vielä tiedossa mitä mahdollisia lisäkenttiä seuraaviin versioihin tulee / " +
-            "mitä mahdollisesti halutaan tähänkin versioon kuitenkinmukaan. " +
-            "Tällä ratkaisulla ei tarvita uusia muuttujia tähän luokkaan toistaiseksi, vaan voidaan pikaisesti toteuttaa muutokset " +
-            "hakemalla tästä Mapista halutut arvot, jos sellaisia tarvitaan. Jatkossa on ehkä syytä lisätä tähän luokkaan omat " +
-            "muuttujansa selvyyden vuoksi (ainakin) niille kentille, jotka ovat pakollisia.")
-    private List<Map<String, String>> customLetterContents;
+	@ApiModelProperty(value = "Koekutsukirjeen leipäteksti, käyttäjältä HTML-muodossa, ajetaan palvelussa JSoup.clean-filtterin läpi", required = true, notes = "Tämä kenttä on turhaan jokaisen kirjeen osa. Olisi hyvä siirtää osaksi luokkaa <code>KoekutsukirjeBatch<code>")
+	private String letterBodyText;
 
-    public AddressLabel getAddressLabel() {
-        return addressLabel;
-    }
+	@ApiModelProperty(value = "Valinnoista tuleva tieto siitä mihin (kouluun, koulutusohjelmaan jne. ) kokeella haetaan.", required = true)
+	private String hakukohde;
 
-    public String getLetterBodyText() {
-        return letterBodyText;
-    }
+	@ApiModelProperty(value = "Valinnoista tuleva tieto siitä mihin (kouluun, koulutusohjelmaan jne. ) kokeella haetaan.", required = false)
+	private String tarjoaja;
 
-    public String getHakukohde() {
-    	return hakukohde;
-    }
+	@ApiModelProperty(value = "Reserved for future", required = false, notes = "Placeholder toistaiseksi. Voidaan välittää avain-arvo-pareina mahdollisia lisätietoja, "
+			+ "joita voidaan käyttää mahdollisesti jatkossa esimerkiksi _hakukohteen_ tai "
+			+ "_kokeen_ tietojen täyttämiseksi koekutsukirjeeseen. Tässä vaiheessa (20140124, versio 8?), kun virkailijat saavat "
+			+ "kirjoittaa koko kirjeen editorilla, ei ole vielä tiedossa mitä mahdollisia lisäkenttiä seuraaviin versioihin tulee / "
+			+ "mitä mahdollisesti halutaan tähänkin versioon kuitenkinmukaan. "
+			+ "Tällä ratkaisulla ei tarvita uusia muuttujia tähän luokkaan toistaiseksi, vaan voidaan pikaisesti toteuttaa muutokset "
+			+ "hakemalla tästä Mapista halutut arvot, jos sellaisia tarvitaan. Jatkossa on ehkä syytä lisätä tähän luokkaan omat "
+			+ "muuttujansa selvyyden vuoksi (ainakin) niille kentille, jotka ovat pakollisia.")
+	private List<Map<String, String>> customLetterContents;
 
-    public List<Map<String, String>> getCustomLetterContents() {
-        return customLetterContents;
-    }
+	public AddressLabel getAddressLabel() {
+		return addressLabel;
+	}
 
-    public String getLanguageCode() {
-        return languageCode;
-    }
+	public String getLetterBodyText() {
+		return letterBodyText;
+	}
 
-    @Override
-    public String toString() {
-        return "Koekutsukirje ["
-        		+ "addressLabel=" + addressLabel
-                + ", languageCode=" + languageCode
-                + ", hakukohde=" + hakukohde
-                + ", letterBodyText=" + letterBodyText 
-                + "]";
-    }
+	public String getHakukohde() {
+		return hakukohde;
+	}
+
+	public String getTarjoaja() {
+		return tarjoaja;
+	}
+
+	public List<Map<String, String>> getCustomLetterContents() {
+		return customLetterContents;
+	}
+
+	public String getLanguageCode() {
+		return languageCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Koekutsukirje [" + "addressLabel=" + addressLabel
+				+ ", languageCode=" + languageCode + ", hakukohde=" + hakukohde
+				+ ", letterBodyText=" + letterBodyText + "]";
+	}
 }
