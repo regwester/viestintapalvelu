f274f848252048b185cffdbc88e0bbb3ae72f8fb
vmheikkinen <veli-matti.heikkinen@cybercom.com>
2014-03-17 10:19:38 +0200

    - Asetettu taulukon sarakkeille tyylit leveyksiä varten - Listan sivutus 5 -> 10 riviin - Komentteja lisätty

--- ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/reportingApp/js/reportedMessageServices.js
+++ ryhmasahkoposti/ryhmasahkoposti-app/src/main/webapp/reportingApp/js/reportedMessageServices.js
@@ -1,25 +1,30 @@
 'use strict';
 
+// Hakee REST-rajapinnan avulla raportoitavan viestin ja vastaanottajat sivutettuna ja lajiteltuna
 reportingApp.factory('ReportedMessageAndRecipients', function($resource) {
 	return $resource("/ryhmasahkoposti-service/reportMessages/vwp/:messageID", {messageID: '@messageID'});
 });
 
+// Hakee REST-rajapinnan avulla raportoitavan viestin tiedot vastaanottajineen, joille viestin lähetys epäonnistui
 reportingApp.factory('ReportedMessageAndRecipientsSendingUnsuccesful', function($resource) {
 	return $resource("/ryhmasahkoposti-service/reportMessages/failed/:messageID", {messageID: '@messageID'});
 });
 
+// Hakee REST-rajapinnan avulla kaikki raportoitavat viestit
 reportingApp.factory('GetReportedMessages', function($resource) {
     return $resource("/ryhmasahkoposti-service/reportMessages/list", {}, {
     	get: {method: "GET", isArray: false}
     });	
 });
 
+// Hakee REST-rajapinnan avulla hakuparametrin mukaiset raportoitavat viestit 
 reportingApp.factory('GetReportedMessagesBySearchArgument', function($resource) {
 	return $resource("/ryhmasahkoposti-service/reportMessages/search/:searchArgument", {}, {
 		get: {method: "GET", params: {searchArgument: '@searchArgument'}, isArray: false}
     });
 });
 
+// Hae raportoitava viesti ja vastaanottajat
 reportingApp.factory('GetReportedMessageAndRecipients', function($route, $q, ReportedMessageAndRecipients) {
 	return function() {
 	    var delay = $q.defer();
@@ -32,6 +37,7 @@ reportingApp.factory('GetReportedMessageAndRecipients', function($route, $q, Rep
 	};
 });
 
+// Hae raportoitava viesti ja vastaanottajat, joille lähetys on epäonnistunut
 reportingApp.factory('GetReportedMessageAndRecipientsSendingUnsuccesful', function($route, $q, 
 	ReportedMessageAndRecipientsSendingUnsuccesful) {
 	return function() {
