21179b718ef3de9f5d055dad1a974c9bb1c1e63e
gardziella <mikael.gardziella@cybercom.com>
2014-04-02 12:50:31 +0300

    Added tables for letter send.

---
+++ viestintapalvelu-service/src/main/resources/db/migration/V005__change_content.sql
@@ -0,0 +1,73 @@
+ALTER TABLE kirjeet.kirjelahetys add CONSTRAINT kirjelahetys_pk PRIMARY KEY (id);
+
+
+-- Table: kirjeet.lahetyskorvauskentat
+
+-- DROP TABLE kirjeet.lahetyskorvauskentat;
+
+CREATE TABLE kirjeet.lahetyskorvauskentat
+(
+  id bigint NOT NULL,
+  version bigint,
+  kirjelahetys_id bigint,
+  nimi character varying(255),
+  oletus_arvo character varying(3000),
+  aikaleima time with time zone,
+  pakollinen boolean,
+  CONSTRAINT lahetyskorvauskentat_pk PRIMARY KEY (id),
+  CONSTRAINT lahetyskorvauskentat_kirjelahetys_id_fkey FOREIGN KEY (kirjelahetys_id)
+      REFERENCES kirjeet.kirjelahetys (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE kirjeet.lahetyskorvauskentat
+  OWNER TO oph;
+
+  
+-- Table: kirjeet.vastaanottaja
+
+-- DROP TABLE kirjeet.vastaanottaja;
+
+CREATE TABLE kirjeet.vastaanottaja
+(
+  id bigint NOT NULL,
+  version bigint,
+  kirjelahetys_id bigint,
+  aikaleima time with time zone,
+  CONSTRAINT vastaanottaja_pk PRIMARY KEY (id),
+  CONSTRAINT vastaanottaja_kirjelahetys_id_fkey FOREIGN KEY (kirjelahetys_id)
+      REFERENCES kirjeet.kirjelahetys (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE kirjeet.vastaanottaja
+  OWNER TO oph;
+
+  
+-- Table: kirjeet.vastaanottajakorvauskentat
+
+-- DROP TABLE kirjeet.vastaanottajakorvauskentat;
+
+CREATE TABLE kirjeet.vastaanottajakorvauskentat
+(
+  id bigint NOT NULL,
+  version bigint,
+  vastaanottaja_id bigint,
+  nimi character varying(255),
+  oletus_arvo character varying(3000),
+  pakollinen boolean,
+  aikaleimia time with time zone,
+  CONSTRAINT vastaanottajakorvauskentat_pk PRIMARY KEY (id),
+  CONSTRAINT vastaanottajakorvauskentat_vastaanottaja_id_fkey FOREIGN KEY (vastaanottaja_id)
+      REFERENCES kirjeet.vastaanottaja (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE kirjeet.vastaanottajakorvauskentat
+  OWNER TO oph;
