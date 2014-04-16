dce04aa7f98fc905cda46c030546e3bb790402b5
jkorkalainen <juha.korkalainen@iki.fi>
2014-04-16 17:05:32 +0300

    corrected filename

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/Constants.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String KOEKUTSUKIRJE_TEMPLATE = "/koekutsukirje_{LANG}.html";
     public static final String LETTER_TEMPLATE = "/letter_{LANG}.html";
     public static final String IPOST_TEMPLATE = "/ipost.xml";
-    public static final String LETTER_IPOST_TEMPLATE = "/letteripost.xml";
+    public static final String LETTER_IPOST_TEMPLATE = "/letter_ipost.xml";
     public static final Charset UTF_8 = Charset.forName("UTF-8");
     public static final Boolean IPOST_TEST = Boolean.FALSE;
 }
