b089375623540fbbc99de34ba1ab619b82d01fb1
gardziella <mikael.gardziella@cybercom.com>
2014-04-29 08:15:27 +0300

    Bug fix in constructor of LetterServiceImpl.

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/letter/impl/LetterServiceImpl.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/letter/impl/LetterServiceImpl.java
@@ -79,7 +79,9 @@ public class LetterServiceImpl implements LetterService {
     @Autowired
     public LetterServiceImpl(LetterBatchDAO letterBatchDAO, LetterReceiverLetterDAO letterReceiverLetterDAO,
         CurrentUserComponent currentUserComponent) {
-
+    	this.letterBatchDAO = letterBatchDAO;
+    	this.letterReceiverLetterDAO = letterReceiverLetterDAO;
+    	this.currentUserComponent = currentUserComponent;
     }
 
     /* ---------------------- */
