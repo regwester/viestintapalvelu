099f8168910259d187627517fa8a9e45e10fdaea
gardziella <mikael.gardziella@cybercom.com>
2014-05-14 13:45:55 +0300

    New jalkkiohjauskirje and attacment templates.

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/dao/impl/DraftDAOImpl.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/dao/impl/DraftDAOImpl.java
@@ -5,21 +5,21 @@ import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
+import com.mysema.query.jpa.impl.JPAQuery;
+import com.mysema.query.types.EntityPath;
+import com.mysema.query.types.expr.BooleanExpression;
+
 import fi.vm.sade.generic.dao.AbstractJpaDAOImpl;
 import fi.vm.sade.viestintapalvelu.dao.DraftDAO;
 import fi.vm.sade.viestintapalvelu.model.Draft;
+import fi.vm.sade.viestintapalvelu.model.QDraft;
 
 @Repository
 public class DraftDAOImpl extends AbstractJpaDAOImpl<Draft, Long> implements DraftDAO {
 
 	public Draft findDraftByNameOrgTag(	String templateName, String templateLanguage, String organizationOid, 
 										String applicationPeriod, String fetchTarget, String tag) {
-		EntityManager em = getEntityManager();
-		
-//		SELECT * FROM kirjeet.luonnos WHERE kirjepohjan_nimi = 'koekutsukirje' AND kirjepohjan_kielikoodi='FI'
-//				AND oid_organisaatio LIKE '%%'
-//				AND haku LIKE '%%' AND hakukohde LIKE '%%' AND tunniste LIKE '%%';
-		
+		EntityManager em = getEntityManager();		
 		
 		String findDraft = "SELECT a FROM Draft a WHERE "
 													+ "a.templateName = :templateName AND "
@@ -51,4 +51,35 @@ public class DraftDAOImpl extends AbstractJpaDAOImpl<Draft, Long> implements Dra
 		return draft;
 	}
 
+	public Draft findDraftByNameOrgTag2(String templateName, String templateLanguage, String organizationOid, 
+										String applicationPeriod, String fetchTarget, String tag) {
+        QDraft draft = QDraft.draft;
+        
+        BooleanExpression whereExpression = draft.templateName.eq(templateName)
+        									.and(draft.templateLanguage.eq(templateLanguage)
+        									.and(draft.organizationOid.eq(organizationOid)));
+        
+        if (!"".equals(applicationPeriod)) {        	
+        	whereExpression = whereExpression.and(draft.applicationPeriod.eq(applicationPeriod));
+        }
+        if (!"".equals(fetchTarget)) {        	
+        	whereExpression = whereExpression.and(draft.fetchTarget.eq(fetchTarget));
+        }
+        if (!"".equals(tag)) {        	
+        	whereExpression = whereExpression.and(draft.tag.eq(tag));
+        }
+//        OrderSpecifier<?> orderBy = orderBy(draft.timestamp);
+        
+        
+        		
+		
+		
+		return new Draft();
+	}
+	
+	protected JPAQuery from(EntityPath<?>... o) {
+        return new JPAQuery(getEntityManager()).from(o);
+    }
+	
+	
 }
