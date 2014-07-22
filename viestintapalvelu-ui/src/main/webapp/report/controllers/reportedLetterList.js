c10e7e058ad266a844024e84d3c3adb87eafe06c
Jaakko Hynynen <jaakko.hynynen@gofore.com>
2014-07-22 18:31:04 +0300

    VIES-175: Haetaan kirjeraportit ja naytetaan ne

--- viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterList.js
+++ viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterList.js
@@ -1,6 +1,42 @@
 'use strict';
 
 angular.module('report')
-.controller('ReportedLetterListCtrl', ['$scope', function($scope) {
+.controller('ReportedLetterListCtrl', ['$scope', '$http', function($scope, $http) {
+  var reportedLettersUrl = '/viestintapalvelu/api/v1/reporting/list';
+
+  $scope.pagination = {
+    page: 1,
+    pageSize: 10
+  };
+
+  // used to make sure the right option is selected after fetch happens
+  function setSelectedOrganizationOid() {
+    var index = $scope.reportedLettersDTO.selectedOrganization
+      , org = $scope.reportedLettersDTO.organizations[index];
+    $scope.selectedOrganizationOid = org.oid;
+  }
+
+  $scope.fetch = function() {
+    var params = {};
+    if ($scope.organisationOid) {
+      params.orgOid = $scope.organisationOid;
+    }
+
+    angular.extend(params, {
+      page: $scope.pagination.page,
+      nbrofrows: $scope.pagination.pageSize
+    });
+
+    $http.get(reportedLettersUrl, {params: params})
+      .success(function(reportedLettersDTO) {
+        $scope.reportedLettersDTO = reportedLettersDTO;
+        setSelectedOrganizationOid();
+      })
+      .error(function(err) {
+        console.log(err);
+      });
+  };
+
+  $scope.fetch();
 
 }]);
