93cc73cf7fb42b578e3d87df3012ec8d02497d46
Jaakko Hynynen <jaakko.hynynen@gofore.com>
2014-07-23 16:53:04 +0300

    VIES-175: Haun kytkeminen backendiin

--- viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterList.js
+++ viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterList.js
@@ -2,7 +2,8 @@
 
 angular.module('report')
 .controller('ReportedLetterListCtrl', ['$scope', '$http', function($scope, $http) {
-  var reportedLettersUrl = '/viestintapalvelu/api/v1/reporting/list';
+  var reportedLettersListUrl = '/viestintapalvelu/api/v1/reporting/list',
+      reportedLettersSearchUrl = '/viestintapalvelu/api/v1/reporting/search';
 
   $scope.pagination = {
     page: 1,
@@ -17,9 +18,13 @@ angular.module('report')
   }
 
   $scope.fetch = function() {
-    var params = {};
+    var params = {}, url = reportedLettersListUrl;
     if ($scope.organisationOid) {
       params.orgOid = $scope.selectedOrganizationOid;
+    }
+    if ($scope.searchArgument) {
+      params.searchArgument = $scope.searchArgument;
+      url = reportedLettersSearchUrl;
     }
 
     angular.extend(params, {
@@ -27,7 +32,7 @@ angular.module('report')
       nbrofrows: $scope.pagination.pageSize
     });
 
-    $http.get(reportedLettersUrl, {params: params})
+    $http.get(url, {params: params})
       .success(function(reportedLettersDTO) {
         $scope.reportedLettersDTO = reportedLettersDTO;
         setSelectedOrganizationOid();
@@ -37,8 +42,20 @@ angular.module('report')
       });
   };
 
+  $scope.search = function() {
+    // reset page for fetch
+    $scope.pagination.page = 1;
+    $scope.fetch(); 
+  };
+
   $scope.showReportedLetter = function(letterBatch) {
     console.log(letterBatch);
+  };
+
+  $scope.emptySearch = function() {
+    $scope.searchArgument = '';
+    $scope.pagination.page = 1;
+    $scope.fetch();
   };
 
   $scope.fetch();
