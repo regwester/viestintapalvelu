08e96e692d60344b103fb316267ce4c7ef4d4b65
Jaakko Hynynen <jaakko.hynynen@gofore.com>
2014-07-25 16:33:17 +0300

    VIES-178:kirjeen lahetysraportin sisaltoa

--- viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterView.js
+++ viestintapalvelu-ui/src/main/webapp/report/controllers/reportedLetterView.js
@@ -1,10 +1,50 @@
 'use strict';
 
 angular.module('report')
-.controller('ReportedLetterViewCtrl', ['$scope', '$state','$stateParams', function ($scope, $state, $stateParams) {
+.controller('ReportedLetterViewCtrl', ['$scope', '$state','$stateParams', '$http', function ($scope, $state, $stateParams, $http) {
+  var reportedLetterUrl = '/viestintapalvelu/api/v1/reporting/view';
+
+  $scope.pagination = {
+    page: 1,
+    pageSize: 10
+  };
 
   $scope.back = function() {
     $state.go('letter_reports');
-  }
+  };
+
+  $scope.fetch = function() {
+    var params = {
+      id: $stateParams.letterBatchID,
+      page: $scope.pagination.page,
+      nbrofrows: $scope.pagination.pageSize
+    };
+
+    $http.get(reportedLetterUrl, { params: params })
+      .success(function(reportedLetter) {
+        $scope.reportedLetterDTO = reportedLetter;
+      })
+      .error(function(err) {
+        console.log(err);
+      });
+  };
+
+  $scope.getLetterContent = function() {
+    var template, content = '';
+
+    function isContent(candidate) {
+      return candidate.name === 'sisalto';
+    }
+
+    if ($scope.reportedLetterDTO && $scope.reportedLetterDTO.template) {
+      template = $scope.reportedLetterDTO.template;
+      if (template.replacements) {
+        content = template.replacements.filter(isContent)[0].defaultValue;
+      }
+    }
+    return content;
+  };
+
+  $scope.fetch();
 
 }]);
