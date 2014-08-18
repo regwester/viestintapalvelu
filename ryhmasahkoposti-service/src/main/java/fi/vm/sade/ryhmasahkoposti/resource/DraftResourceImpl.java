c6cb7c6d64e9945dfdfcdc106c6ed4769a10d780
Wille Keltikangas <wille.keltikangas@gofore.com>
2014-08-18 09:40:02 +0300

    Lisätty geneerinen resurssirajapinta, jotta vältytään turhalta toistolta

--- ryhmasahkoposti-service/src/main/java/fi/vm/sade/ryhmasahkoposti/resource/DraftResourceImpl.java
+++ ryhmasahkoposti-service/src/main/java/fi/vm/sade/ryhmasahkoposti/resource/DraftResourceImpl.java
@@ -19,7 +19,7 @@ import fi.vm.sade.ryhmasahkoposti.api.resource.DraftResource;
 import fi.vm.sade.ryhmasahkoposti.service.DraftService;
 
 @Component
-public class DraftResourceImpl implements DraftResource {
+public class DraftResourceImpl extends GenericResourceImpl implements DraftResource {
     
     private final Logger logger = LoggerFactory.getLogger(DraftResourceImpl.class.getName());
     
@@ -71,49 +71,17 @@ public class DraftResourceImpl implements DraftResource {
         }
     }
     
-    public String saveDraft(Draft draft) { //TODO: validate that organization oid in draft matches user oid, or better yet, simply ignore the given oid
+    public String saveDraft(Draft draft) {
         if(draft == null) {
             throwError400("Draft is not defined");
         }
         //Reset the time (might not be the best solution)
         draft.setCreateDate(new DateTime());
         //Set the user oid (again, overwriting things might not be the most elegant solution)
-        //TODO: fix inconsistensy with the naming
-        draft.setOrganizationOid(getCurrentUserOid());
+        draft.setUserOid(getCurrentUserOid());
 
         logger.debug("Draft: ", draft.toString());
         return draftService.saveDraft(draft);
     }
-    
-    private void throwError404(String msg) {
-        throwError(Response.Status.NOT_FOUND, msg);
-    }
-    
-    private void throwError400(String msg) {
-        throwError(Response.Status.BAD_REQUEST, msg);
-    }
-    
-    private void throwError500(String msg) {
-        throwError(Response.Status.INTERNAL_SERVER_ERROR, msg);
-    }
-    
-    private void throwError(Response.Status status, String msg) {
-        throw new WebApplicationException(Response.status(status)
-                .type(MediaType.TEXT_PLAIN).entity(msg).build());
-    }
 
-    /**
-     * Fetches user oid from SecurityContext
-     *
-     * @return oid of currently authenticated user
-     * @throws NullPointerException
-     *             if user oid is not available from security context
-     */
-    protected String getCurrentUserOid() throws NullPointerException { //Should this be placed in a static util function?
-        String oid = SecurityContextHolder.getContext().getAuthentication().getName();
-        if (oid == null) {
-            throw new NullPointerException("No user name available from SecurityContext!");
-        }
-        return oid;
-    }
 }
