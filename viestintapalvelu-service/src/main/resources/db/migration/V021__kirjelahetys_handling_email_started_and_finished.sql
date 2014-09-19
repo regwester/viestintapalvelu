1bad34c667ddf73ef7fe590e00530c6035945136
Humakt83 <risto.salama@cybercom.com>
2014-09-19 11:44:48 +0300

    Change version of flyway scripts in the branch

--- viestintapalvelu-service/src/main/resources/db/migration/V021__kirjelahetys_handling_email_started_and_finished.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE kirjeet.kirjelahetys ADD COLUMN email_kasittely_aloitettu timestamp without time zone;
-ALTER TABLE kirjeet.kirjelahetys ADD COLUMN email_kasittely_valmis timestamp without time zone;
--- viestintapalvelu-service/src/main/resources/db/migration/V023__vastaanottaja_haluttu_kieli.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE kirjeet.vastaanottaja ADD COLUMN haluttukieli character varying(5);
