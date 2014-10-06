c8dc96e7b82e933c5f658a53ba9d9a454c5a8bc6
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2014-10-06 13:40:27 +0300

    Korjattu revisio

--- viestintapalvelu-service/src/main/resources/db/migration/V037__add_type_to_letter_batch_processing_error.sql
+++ viestintapalvelu-service/src/main/resources/db/migration/V037__add_type_to_letter_batch_processing_error.sql
@@ -3,7 +3,7 @@ alter table kirjeet.kirjelahetysvirhe add column tyyppi varchar(64);
 update kirjeet.kirjelahetysvirhe set tyyppi = 'LETTER';
 alter table kirjeet.kirjelahetysvirhe alter column tyyppi set not null;
 alter table kirjeet.kirjelahetysvirhe add constraint virhe_tyyppi check(
-  'LETTER', 'IPOSTI', 'EMAIL'
+  tyyppi in('LETTER', 'IPOSTI', 'EMAIL')
 );
 alter table kirjeet.kirjelahetysvirhe alter column vastaanottaja_id drop not null;
 alter table kirjeet.kirjelahetysvirhe add constraint vastaanottaja_id_null check(
