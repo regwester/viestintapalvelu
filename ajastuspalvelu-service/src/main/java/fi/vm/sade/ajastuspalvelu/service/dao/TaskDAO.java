b338bafec89e490be0a782380176bcaf01f7cbc2
Humakt83 <risto.salama@cybercom.com>
2014-10-23 14:25:50 +0300

    TaskDAO and ScheduledRunDAO

---
+++ ajastuspalvelu-service/src/main/java/fi/vm/sade/ajastuspalvelu/service/dao/TaskDAO.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.ajastuspalvelu.service.dao;
+
+
+
+import fi.vm.sade.ajastuspalvelu.service.model.Task;
+import fi.vm.sade.generic.dao.AbstractJpaDAOImpl;
+
+public class TaskDAO extends AbstractJpaDAOImpl<Task, Long> {
+    
+}
