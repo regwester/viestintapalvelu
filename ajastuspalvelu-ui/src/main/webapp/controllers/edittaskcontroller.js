dcbae670a90ac191b911cc8ab38bddd111f9b7a0
Humakt83 <risto.salama@cybercom.com>
2014-10-28 18:49:09 +0200

    Initial basic implementation of delete

--- ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
+++ ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
@@ -1,3 +1,5 @@
+'use strict';
+
 app.factory('EditScheduledTask', function($resource) {
     return $resource(SERVICE_REST_PATH + "scheduledtask", {}, {
         put : {
@@ -5,14 +7,30 @@ app.factory('EditScheduledTask', function($resource) {
         }
     });
 });
+    
+app.factory('RemoveScheduledTask', function($resource) {
+    return $resource(SERVICE_REST_PATH + "scheduledtask/close/:scheduledtaskid", {
+	scheduledtaskid : "@scheduledtaskid"
+    }, {
+        remove : {
+            method : "PUT"
+        }
+    });
+});
 
-app.controller('EditTaskController', ['$scope', '$location', '$routeParams', 'EditScheduledTask', 
-                                      function($scope, $location, $routeParams, EditScheduledTask) {
-    $scope.task = $routeParams.task
+app.controller('EditTaskController', ['$scope', '$location', '$routeParams', 'EditScheduledTask', 'RemoveScheduledTask', 
+                                      function($scope, $location, $routeParams, EditScheduledTask, RemoveScheduledTask) {
+    $scope.task = $routeParams.task;
     
     $scope.save = function() {
 	EditScheduledTask.put({}, $scope.task, function(result) {
-	            $location.path("/etusivu/");
+	    $location.path("/etusivu/");
+	});
+    }
+    
+    $scope.remove = function() {
+	RemoveScheduledTask.remove({ scheduledtaskid : $scope.task.id},{}, function(result) {
+	    $location.path("/etusivu/");
 	});
     }
 }]);
