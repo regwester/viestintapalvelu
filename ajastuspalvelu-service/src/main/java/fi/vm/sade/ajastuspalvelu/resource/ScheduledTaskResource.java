0bffdff194824d8c7efef19cac5bcdf9a81ee25b
Humakt83 <risto.salama@cybercom.com>
2014-10-31 10:49:01 +0200

    Consumes and produces

--- ajastuspalvelu-service/src/main/java/fi/vm/sade/ajastuspalvelu/resource/ScheduledTaskResource.java
+++ ajastuspalvelu-service/src/main/java/fi/vm/sade/ajastuspalvelu/resource/ScheduledTaskResource.java
@@ -2,11 +2,13 @@ package fi.vm.sade.ajastuspalvelu.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -14,6 +16,7 @@ import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import fi.vm.sade.ajastuspalvelu.service.ScheduledTaskService;
@@ -29,6 +32,7 @@ public class ScheduledTaskResource {
     private ScheduledTaskService service;
     
     @GET
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
     @Path("/{scheduledtaskid}")
     public Response getScheduledTask(@PathParam("scheduledtaskid") Long scheduledTaskId) {
         ScheduledTaskDto dto = service.findById(scheduledTaskId);
@@ -36,19 +40,24 @@ public class ScheduledTaskResource {
     }
     
     @GET
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
     @Path("/list")
     public Response getScheduledTasks() {
-        List<ScheduledTaskDto> dto = service.list();
-        return Response.status(Status.OK).entity(dto).build();
+        List<ScheduledTaskDto> dtos = service.list();
+        return Response.status(Status.OK).entity(dtos).build();
     }
     
     @POST
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    @Produces(MediaType.TEXT_PLAIN_VALUE)
     public Response insertScheduledTask(ScheduledTaskDto dto) {
         service.insert(dto);
         return Response.status(Status.OK).build();
     }
-    
+
     @PUT
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    @Produces(MediaType.TEXT_PLAIN_VALUE)
     public Response update(ScheduledTaskDto dto) {
         service.update(dto);
         return Response.status(Status.OK).build();
@@ -56,6 +65,7 @@ public class ScheduledTaskResource {
     
     @PUT
     @Path("/close/{scheduledtaskid}")
+    @Produces(MediaType.TEXT_PLAIN_VALUE)
     public Response close(@PathParam("scheduledtaskid") Long id) {
         try {
             service.remove(id);
