a548250da140ffd728290dddf23fd6322b910fbe
Humakt83 <risto.salama@cybercom.com>
2014-10-31 10:50:25 +0200

    Use actual scheduledtask in editcontroller

--- ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
+++ ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
@@ -1,14 +1,14 @@
 'use strict';
 
-app.factory('EditScheduledTask', function($resource) {
+app.factory('EditScheduledTask', ['$resource', function($resource) {
     return $resource(SERVICE_REST_PATH + "scheduledtask", {}, {
         put : {
             method : "PUT"
         }
     });
-});
+}]);
     
-app.factory('RemoveScheduledTask', function($resource) {
+app.factory('RemoveScheduledTask', ['$resource', function($resource) {
     return $resource(SERVICE_REST_PATH + "scheduledtask/close/:scheduledtaskid", {
 	scheduledtaskid : "@scheduledtaskid"
     }, {
@@ -16,11 +16,26 @@ app.factory('RemoveScheduledTask', function($resource) {
             method : "PUT"
         }
     });
-});
+}]);
 
-app.controller('EditTaskController', ['$scope', '$location', '$routeParams', 'EditScheduledTask', 'RemoveScheduledTask', 
-                                      function($scope, $location, $routeParams, EditScheduledTask, RemoveScheduledTask) {
-    $scope.task = $routeParams.task;
+app.factory('FetchScheduledTask', ['$resource', function($resource) {
+    return $resource(SERVICE_REST_PATH + "scheduledtask/:scheduledtaskid", {
+	scheduledtaskid : "@scheduledtaskid"
+    }, {
+        get : {
+            method : "GET"
+        }
+    });
+}]);
+
+app.controller('EditTaskController', ['$scope', '$location', '$routeParams', 'EditScheduledTask', 'RemoveScheduledTask', 'FetchScheduledTask', 
+                                      function($scope, $location, $routeParams, EditScheduledTask, RemoveScheduledTask, FetchScheduledTask) {
+    
+    
+    FetchScheduledTask.get( scheduledtaskid : $routeParams.task ), {}, function(result) {
+	$scope.task = result;
+    });
+    
     
     $scope.save = function() {
 	EditScheduledTask.put({}, $scope.task, function(result) {
@@ -29,7 +44,7 @@ app.controller('EditTaskController', ['$scope', '$location', '$routeParams', 'Ed
     }
     
     $scope.remove = function() {
-	RemoveScheduledTask.remove({ scheduledtaskid : $scope.task.id},{}, function(result) {
+	RemoveScheduledTask.remove({ scheduledtaskid : $scope.task.id },{}, function(result) {
 	    $location.path("/etusivu/");
 	});
     }
