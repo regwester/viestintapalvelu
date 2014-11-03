bd4bb1648381db165ce8c5f03206cd90840ed7ff
Humakt83 <risto.salama@cybercom.com>
2014-11-03 17:07:22 +0200

    Options tp pick date and time for scheduled task

--- ajastuspalvelu-ui/src/main/webapp/controllers/createtaskcontroller.js
+++ ajastuspalvelu-ui/src/main/webapp/controllers/createtaskcontroller.js
@@ -6,17 +6,46 @@ app.factory('CreateScheduledTask', function($resource) {
     });
 });
 
-app.controller('CreateTaskController', ['$scope', '$location', 'CreateScheduledTask', function($scope, $location, CreateScheduledTask) {
+app.controller('CreateTaskController', ['$scope', '$location', 'CreateScheduledTask', 'Hakus', 'Tasks', 
+                                        function($scope, $location, CreateScheduledTask, Hakus, Tasks) {
+    
+    Hakus.get({}, function(result) {
+	$scope.hakus = result
+	$scope.selectedHaku = result[0]
+    });
+
+    Tasks.get({}, function(result) {
+	$scope.tasks = result
+	$scope.selectedTask = result[0]
+    });
+    
+    $scope.selectedDate = new Date();
+    $scope.selectedTime = new Date();
     
     $scope.create = function() {
+	var dateSelected = $scope.selectedDate
+	dateSelected.setHours($scope.selectedTime.getHours)
+	dateSelected.setMinutes($scope.selectedTime.getMinutes)
 	var task = {
-		taskName: $scope.selectedTask,
+		taskId: $scope.selectedTask.id,
 		hakuOid: $scope.selectedHaku.oid,
-		runtimeForSingle: $scope.runtimeForSingle
+		runtimeForSingle: dateSelected
 	}
 	CreateScheduledTask.post({}, task, function(result) {
 	            $location.path("/etusivu/");
 	});
+    }
+    
+    $scope.openCalendar = function($event) {
+	$event.preventDefault();
+	$event.stopPropagation();
+	$scope.opened = true;
+    };
+    
+    $scope.hakuByName = function(haku) {
+	var name = haku.nimi['kieli_fi']
+	if (!name || name == '') name = haku.nimi['kieli_en']
+	return name
     }
     
 }]);
