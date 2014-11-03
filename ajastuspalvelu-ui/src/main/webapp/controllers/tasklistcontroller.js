1d13c9a53412e937d01ad359abace30f0024cc5f
Humakt83 <risto.salama@cybercom.com>
2014-11-03 17:41:58 +0200

    Format datetime and haku in front page

--- ajastuspalvelu-ui/src/main/webapp/controllers/tasklistcontroller.js
+++ ajastuspalvelu-ui/src/main/webapp/controllers/tasklistcontroller.js
@@ -7,9 +7,14 @@ app.factory('ScheduledTasks', function($resource) {
     });
 });
 
-app.controller('TaskListController', ['$scope', '$location', 'ScheduledTasks', function($scope, $location, ScheduledTasks) {
-    ScheduledTasks.get({}, function(result) {
-	$scope.tasks = result
+app.controller('TaskListController', ['$scope', '$location', '$filter', 'ScheduledTasks', 'Hakus', 
+                                      function($scope, $location, $filter, ScheduledTasks, Hakus) {
+    Hakus.get({}, function(result) {
+	$scope.hakus = result
+	
+	ScheduledTasks.get({}, function(result) {
+	    $scope.tasks = result
+	});
     });
     
     $scope.create = function() {
@@ -18,5 +23,12 @@ app.controller('TaskListController', ['$scope', '$location', 'ScheduledTasks', f
     
     $scope.edit = function(task) {
 	$location.path("/edit/" + task)
+    }
+    
+    $scope.hakuByName = function(hakuOid) {
+	var haku = $filter('filter') ($scope.hakus, function (haku) {return haku.oid === hakuOid})[0]
+	var name = haku.nimi['kieli_fi']
+	if (!name || name == '') name = haku.nimi['kieli_en']
+	return name
     }
 }]);
