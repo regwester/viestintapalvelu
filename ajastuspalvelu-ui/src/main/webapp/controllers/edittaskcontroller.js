438ccfd14aa8500ba9d7a8d4661497c1c980a2fe
Humakt83 <risto.salama@cybercom.com>
2014-11-04 13:30:05 +0200

    Set time for edited task

--- ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
+++ ajastuspalvelu-ui/src/main/webapp/controllers/edittaskcontroller.js
@@ -33,6 +33,8 @@ app.controller('EditTaskController', ['$scope', '$location', '$routeParams', '$f
     
     FetchScheduledTask.get( {scheduledtaskid : $routeParams.task} , {}, function(result) {
 	$scope.task = result
+	$scope.selectedDate = $scope.task.runtimeForSingle
+	$scope.selectedTime = $scope.task.runtimeForSingle
 	
 	Hakus.get({}, function(result) {
 		$scope.hakus = result
@@ -48,7 +50,9 @@ app.controller('EditTaskController', ['$scope', '$location', '$routeParams', '$f
     $scope.save = function() {
 	var taskToSave = $scope.task
 	taskToSave.taskId = $scope.selectedTask.id
-	taskToSave.hakuOid = $scope.selectedHaku.hakuOid
+	taskToSave.hakuOid = $scope.selectedHaku.oid
+	var dateSelected = $filter('date')($scope.selectedDate, 'yyyy-MM-dd')
+	dateSelected += "T" + $filter('date')($scope.selectedTime, 'HH:mm:ss.sss')
 	EditScheduledTask.put({}, taskToSave, function(result) {
 	    $location.path("/etusivu/");
 	});
@@ -63,4 +67,10 @@ app.controller('EditTaskController', ['$scope', '$location', '$routeParams', '$f
     $scope.hakuByName = function(haku) {
 	return HakuNameByLocale(haku)
     }
+    
+    $scope.openCalendar = function($event) {
+	$event.preventDefault();
+	$event.stopPropagation();
+	$scope.opened = true;
+    };
 }]);
