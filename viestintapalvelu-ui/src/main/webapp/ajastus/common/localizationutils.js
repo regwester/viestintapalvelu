8812da6ff430933d3b2afc57357ccac044ac79d7
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2014-11-24 10:24:32 +0200

    Merge developista.

--- viestintapalvelu-ui/src/main/webapp/ajastus/common/localizationutils.js
+++ viestintapalvelu-ui/src/main/webapp/ajastus/common/localizationutils.js
@@ -1,26 +1,26 @@
-'use strict'
+'use strict';
 
 ajastusApp.factory('HakuNameByLocale', ['$locale', function ($locale) {
-    var localeMapping = new Map([
-        ["fi-fi", "kieli_fi"],
-        ["sv-se", "kieli_sv"],
-        ["en-us", "kieli_en"]
-    ]);
+    var localeMapping = {
+        "fi-fi": "kieli_fi",
+        "sv-se": "kieli_sv",
+        "en-us": "kieli_en"
+    };
 
     var lang = (!$locale.id) ? $locale.id : "fi_fi";
 
     return function (haku) {
         function getAnyName(haku) {
             var anyName = null;
-            localeMapping.forEach(function (value) {
+            angular.forEach(localeMapping, function (value, key) {
                 if (!anyName || anyName == '') {
-                    anyName = haku.nimi[value]
+                    anyName = haku.nimi[key] || haku.nimi[value];
                 }
             });
             return anyName
         }
 
-        var name = haku.nimi[localeMapping.get(lang)];
+        var name = haku.nimi[localeMapping[lang]];
         if (!name || name == '') name = getAnyName(haku);
         return name;
     }
