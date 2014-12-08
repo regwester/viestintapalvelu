aebcb84de00b87ece2075db430ebcfb13101b447
Joni Mäkelä <joni.makela@gofore.com>
2014-12-08 13:03:22 +0200

    VIES-246 Moved organization hierarchy querying to its own service

--- viestintapalvelu-ui/src/main/develop/letter-templates/services/template.js
+++ viestintapalvelu-ui/src/main/develop/letter-templates/services/template.js
@@ -16,9 +16,8 @@ angular.module('letter-templates')
             getHakus: function() {
                 return $http.get(serviceUrl + 'options/hakus');
             },
-
-            getByApplicationPeriod: function (applicationPeriod) {
-                return $http.get(templateBaseUrl + 'listByApplicationPeriod/' + applicationPeriod);
+            getTemplatesByOid: function(oidList) {
+                return $http.get(templateBaseUrl+"byOrganizationOid", {params: {oids: oidList}})
             },
             getApplicationTargets: function() {
                 return deferred.promise;
@@ -34,50 +33,6 @@ angular.module('letter-templates')
             },
             publishTemplate : function() {
                 return true;
-            },
-            getDefaultTemplates: function(status) {
-                return $http.get(templateBaseUrl + 'defaults', {params: {state: status}});
-            },
-            getParsedTreeGrid : function(response) {
-                console.log("getParsedTreeGrid");
-                console.log(response);
-                var map = function(fn, arr) {
-                    var result = [];
-
-                    arr.forEach(function(element) {
-                        result.push(fn(element)) ;
-                    });
-                    return result;
-                };
-
-
-                var parseData = function(item) {
-                    var firstColum18nStr = "Organisaatio ja kirjetyyppi";
-                    var newRow = {};
-
-                    //TODO handle localization
-                    if(item.nimi && item.nimi.fi) {
-                        newRow[firstColum18nStr] = item.nimi.fi;
-                    } else if (item.nimi && item.nimi.sv) {
-                        newRow[firstColum18nStr] = item.nimi.sv;
-                    } else if (item.nimi && item.nimi.en) {
-                        newRow[firstColum18nStr] = item.nimi.en;
-                    }
-
-                    newRow["lang"] = item.language;
-                    newRow["status"] = item.state;
-                    if(item.children && item.children.length > 0) {
-                        var childrenRows = map(parseData, item.children);
-                        newRow["children"] = childrenRows;
-                    }
-                    return newRow;
-                };
-
-                var newData = [];
-                var dataArr = [];
-                dataArr.push(response.data[0]);
-                newData = map(parseData, dataArr);
-                return newData;
             }
         }
     }]);
