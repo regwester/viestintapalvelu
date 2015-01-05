9161237226825f04e5a4d989a64e24b5a9c042ad
Humakt83 <risto.salama@cybercom.com>
2015-01-05 04:26:59 +0200

    VIES-273:  Convert receivers for asiointitili

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToAsiointiTiliConverter.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToAsiointiTiliConverter.java
@@ -15,9 +15,16 @@
  */
 package fi.vm.sade.viestintapalvelu.message.conversion;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+
 import fi.vm.sade.viestintapalvelu.api.message.MessageData;
+import fi.vm.sade.viestintapalvelu.api.message.Receiver;
+import fi.vm.sade.viestintapalvelu.asiontitili.api.dto.AsiointitiliMessageDto;
 import fi.vm.sade.viestintapalvelu.asiontitili.api.dto.AsiointitiliSendBatchDto;
 
 /**
@@ -27,16 +34,31 @@ import fi.vm.sade.viestintapalvelu.asiontitili.api.dto.AsiointitiliSendBatchDto;
 @Component
 public class MessageToAsiointiTiliConverter implements MessageDataConverter<MessageData, AsiointitiliSendBatchDto> {
 
-    /* (non-Javadoc)
-     * @see fi.vm.sade.viestintapalvelu.message.conversion.MessageDataConverter#convert(java.lang.Object)
-     */
     @Override
     public AsiointitiliSendBatchDto convert(MessageData data) {
         AsiointitiliSendBatchDto batch = new AsiointitiliSendBatchDto();
         batch.setTemplateName(data.templateName);
         batch.setTemplateReplacements(data.commonReplacements);
         batch.setLanguageCode(data.language);
+        batch.setMessages(convertMessages(data.receivers));
+        //TODO optional fields? are they necessary to convert here?
         return batch;
+    }
+
+    private List<AsiointitiliMessageDto> convertMessages(List<Receiver> receivers) {
+        return Lists.transform(receivers, new Function<Receiver, AsiointitiliMessageDto>() {
+
+            @Override
+            public AsiointitiliMessageDto apply(Receiver input) {
+                AsiointitiliMessageDto dto = new AsiointitiliMessageDto();
+                dto.setAddressLabel(input.addressLabel);
+                dto.setTemplateReplacements(input.replacements);
+                dto.setReceiverHetu(input.hetu);
+                //TODO: dto.setLanguageCode()?
+                return dto;
+            }
+            
+        });
     }
 
 }
--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToAsiointiTiliConverterTest.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToAsiointiTiliConverterTest.java
@@ -1,13 +1,22 @@
 package fi.vm.sade.viestintapalvelu.message.conversion;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Iterables;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import fi.vm.sade.viestintapalvelu.api.address.AddressLabel;
 import fi.vm.sade.viestintapalvelu.api.message.MessageData;
 import fi.vm.sade.viestintapalvelu.api.message.Receiver;
-import static org.junit.Assert.assertNotNull;
+import fi.vm.sade.viestintapalvelu.asiontitili.api.dto.AsiointitiliMessageDto;
+import fi.vm.sade.viestintapalvelu.asiontitili.api.dto.AsiointitiliSendBatchDto;
 
 public class MessageToAsiointiTiliConverterTest {
     
@@ -15,10 +24,33 @@ public class MessageToAsiointiTiliConverterTest {
     
     @Test
     public void convertsToAsiointiTiliSendBatchDto() {
-        assertNotNull(converter.convert(givenMessageData()));
+        MessageData message = givenMessageData();
+        assertBatch(message, converter.convert(message));
+    }
+
+    private void assertBatch(MessageData message, AsiointitiliSendBatchDto batch) {
+        assertEquals(message.templateName, batch.getTemplateName());
+        assertEquals(message.language, batch.getLanguageCode());
+        assertEquals(message.commonReplacements, batch.getTemplateReplacements());
+        assertMessages(message.receivers, batch.getMessages());
     }
     
+    private void assertMessages(final List<Receiver> receivers, List<AsiointitiliMessageDto> messages) {
+        for (final Receiver receiver : receivers) {
+            assertTrue(Iterables.tryFind(messages, new Predicate<AsiointitiliMessageDto>() {
+
+                @Override
+                public boolean apply(AsiointitiliMessageDto input) {
+                    return receiver.addressLabel.equals(input.getAddressLabel()) && receiver.hetu.equals(input.getReceiverHetu()) && receiver.replacements.equals(input.getTemplateReplacements());
+                }
+                
+            }).isPresent());
+        }
+        
+    }
+
     private MessageData givenMessageData() {
-        return new MessageData("templateName", "FI", new ArrayList<Receiver>(), new HashMap<String, Object>());
+        Receiver receiver = new Receiver("test@test.com", new AddressLabel(), new HashMap<String, Object>(), "010101-123N");
+        return new MessageData("templateName", "FI", Arrays.asList(receiver), new HashMap<String, Object>());
     }
 }
