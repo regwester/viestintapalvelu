fad4c0f8799d425c2235f33b3e8ec7db57e5f3a7
jkorkalainen <juha.korkalainen@iki.fi>
2015-01-13 13:52:46 +0200

    copyright + tyylit

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToEmailConverterTest.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/message/conversion/MessageToEmailConverterTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015 The Finnish National Board of Education - Opetushallitus
+ *
+ * This program is free software: Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * European Union Public Licence for more details.
+ */
 package fi.vm.sade.viestintapalvelu.message.conversion;
 
 import java.util.Arrays;
@@ -17,24 +32,23 @@ import fi.vm.sade.viestintapalvelu.api.message.Receiver;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-
 public class MessageToEmailConverterTest {
-    
+
     private MessageDataConverter<MessageData, EmailData> converter = new MessageToEmailConverter();
-    
+
     @Test
     public void convertsToEmail() {
         MessageData message = givenMessageData();
         assertEmail(message, converter.convert(message).wrapped);
     }
-    
+
     private void assertEmail(MessageData message, EmailData email) {
         assertEquals(message.templateName, email.getEmail().getTemplateName());
         assertEquals(message.language, email.getEmail().getLanguageCode());
         assertEquals(message.commonReplacements.size(), email.getReplacements().size());
         assertMessages(message.receivers, email.getRecipient());
     }
-    
+
     private void assertMessages(final List<Receiver> receivers, List<EmailRecipient> list) {
         for (final Receiver receiver : receivers) {
             assertTrue(Iterables.tryFind(list, new Predicate<EmailRecipient>() {
@@ -43,7 +57,7 @@ public class MessageToEmailConverterTest {
                 public boolean apply(EmailRecipient input) {
                     return receiver.email.equals(input.getEmail()) && receiver.language.equals(input.getLanguageCode());
                 }
-                
+
             }).isPresent());
         }
     }
@@ -51,7 +65,7 @@ public class MessageToEmailConverterTest {
     private MessageData givenMessageData() {
         return givenMessageData(Arrays.asList(givenReceiver("010101-123N")));
     }
-    
+
     private MessageData givenMessageData(List<Receiver> receivers) {
         return new MessageData("templateName", "FI", receivers, new HashMap<String, Object>());
     }
