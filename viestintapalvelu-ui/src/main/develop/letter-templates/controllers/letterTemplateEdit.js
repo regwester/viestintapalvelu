44e2ff66cf2f8ecf78e998b6641b94fcfad0790a
jkorkalainen <juha.korkalainen@iki.fi>
2015-01-19 17:24:46 +0200

    preview post method

--- viestintapalvelu-ui/src/main/develop/letter-templates/controllers/letterTemplateEdit.js
+++ viestintapalvelu-ui/src/main/develop/letter-templates/controllers/letterTemplateEdit.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('letter-templates').controller('LetterTemplateEditCtrl',
-    ['$scope', '$state', '$filter', '$modal', '$window', 'Global', 'PersonService', 'TemplateService',
-    function($scope, $state, $filter, $modal, $window, Global, PersonService, TemplateService) {
+    ['$scope', '$http', '$state', '$filter', '$modal', '$window', 'Global', 'PersonService', 'TemplateService',
+    function($scope, $http, $state, $filter, $modal, $window, Global, PersonService, TemplateService) {
 
         $scope.editorOptions = Global.getEditorOptions();
 
         TemplateService.getTemplateByIdAndState($state.params.templateId, 'luonnos').success(function(result) {
             $scope.template = result;
             TemplateService.getStructureById($scope.template.structureId).success(function(structure) {
-        	$scope.contentReplacements = structure.replacements;
+            $scope.contentReplacements = structure.replacements;
             });
             PersonService.getPerson(result.storingOid).success(function(person) {
-        	$scope.saverName = person.firstNames + " " + person.lastName;
+            $scope.saverName = person.firstNames + " " + person.lastName;
             })
             if ($scope.template.applicationPeriods != null && $scope.template.applicationPeriods.length > 0) {
-        	TemplateService.getApplicationTargets().then(function(targets) {
-        	    var target = $filter('filter')(targets, {oid: $scope.template.applicationPeriods[0]});
-        	    var templateLanguage = $scope.template.language;
-        	    $scope.applicationTargetForDisplay = templateLanguage === 'SV' ? target[0].nimi.kieli_sv : templateLanguage === 'EN' ? target[0].nimi.kieli_en : target[0].nimi.kieli_fi;
-        	})
+            TemplateService.getApplicationTargets().then(function(targets) {
+                var target = $filter('filter')(targets, {oid: $scope.template.applicationPeriods[0]});
+                var templateLanguage = $scope.template.language;
+                $scope.applicationTargetForDisplay = templateLanguage === 'SV' ? target[0].nimi.kieli_sv : templateLanguage === 'EN' ? target[0].nimi.kieli_en : target[0].nimi.kieli_fi;
+            })
             }
         }).error(function(result) {
             //TODO handle errors
@@ -32,7 +32,7 @@ angular.module('letter-templates').controller('LetterTemplateEditCtrl',
         
         $scope.save = function() {
             TemplateService.updateTemplate().put({}, $scope.template, function() {
-        	//TODO: some feedback to the user
+            //TODO: some feedback to the user
             });
         };
         
@@ -42,29 +42,53 @@ angular.module('letter-templates').controller('LetterTemplateEditCtrl',
         
         $scope.publish = function() {
             var modalInstance = $modal.open({
-        	templateUrl: 'views/letter-templates/views/partials/publishdialog.html',
-        	controller: 'PublishTemplate',
-        	size: 'sm',
-        	resolve: {
-        	    templateId: function () {
-        		return $scope.template.id
-        	    },
-        	    state: function() {
-        		return $scope.template.state
-        	    },
-        	    template: function () {
-        		return $scope.template
-        	    }
-        	}
+            templateUrl: 'views/letter-templates/views/partials/publishdialog.html',
+            controller: 'PublishTemplate',
+            size: 'sm',
+            resolve: {
+                templateId: function () {
+                return $scope.template.id
+                },
+                state: function() {
+                return $scope.template.state
+                },
+                template: function () {
+                return $scope.template
+                }
+            }
             });
 
             modalInstance.result.then(function() {
-        	$state.go('letter-templates_overview');
+            $state.go('letter-templates_overview');
             });
         };
 
         $scope.previewPDF = function(args) {
-            $window.open("/viestintapalvelu/api/v1/preview/letterbatch/pdf?templateId="+$scope.template.id+"&templateState="+$scope.template.state);
+            var contents = "";
+            //$scope.
+            angular.forEach($scope.template.replacements,function(value,index){
+                if (value.name == 'sisalto') {
+                    contents = value.defaultValue;
+                }
+            });
+            
+            $http({
+                url: '/viestintapalvelu/api/v1/preview/letterbatch/pdf',
+                method: "POST",
+                data: {'templateId' : $scope.template.id,
+                       'templateState' : $scope.template.state,
+                       'letterContent' : contents},
+                headers: {
+                   'Content-type': 'application/json'
+                },
+                responseType: 'arraybuffer'
+            }).success(function (data, status, headers, config) {
+                var blob = new Blob([data], {type: "application/pdf"});
+                var objectUrl = URL.createObjectURL(blob);
+                window.open(objectUrl);
+            }).error(function (data, status, headers, config) {
+
+            });
         };
 
         $scope.previewLetter = function(args) {
