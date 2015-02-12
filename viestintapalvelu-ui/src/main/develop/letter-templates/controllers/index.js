6dd0774daf9fa1b61c2bd0b4575a320640dd5567
jkorkalainen <juha.korkalainen@iki.fi>
2015-02-12 19:42:36 +0200

    letter-templates tyhja sivu ekalla latauksella korjaus

--- viestintapalvelu-ui/src/main/develop/letter-templates/controllers/index.js
+++ viestintapalvelu-ui/src/main/develop/letter-templates/controllers/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('letter-templates')
-    .controller('LetterTemplateIndexCtrl', ['$scope','$state', '$http', function($scope, $state, $http){
-        $http.get("/viestintapalvelu/api/v1/template/ok").success(function (data) {}).error(function(data, status, headers, config) {});
+    .controller('LetterTemplateIndexCtrl', ['$scope','$state', '$http', '$location', function($scope, $state, $http, $location){
+        $http.get("/viestintapalvelu/api/v1/template/ok").success(function (data) {}).error(function(data, status, headers, config) {}).then(function(result) {});
         $scope.isOverviewState = function() {
             return $state.is('letter-templates.overview');
         }
