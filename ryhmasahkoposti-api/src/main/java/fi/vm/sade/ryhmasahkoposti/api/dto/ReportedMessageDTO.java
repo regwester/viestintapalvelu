a142eb03400e852c744d6354228e8ba6f44f4237
Harri Hämäläinen <hhamalai@iki.fi>
2016-01-25 09:31:13 +0200

    Use final Arrays.asList instead of ArrayList

--- ryhmasahkoposti-api/src/main/java/fi/vm/sade/ryhmasahkoposti/api/dto/ReportedMessageDTO.java
+++ ryhmasahkoposti-api/src/main/java/fi/vm/sade/ryhmasahkoposti/api/dto/ReportedMessageDTO.java
@@ -15,7 +15,7 @@
  **/
 package fi.vm.sade.ryhmasahkoposti.api.dto;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReportedMessageDTO extends EmailMessageDTO {
@@ -65,18 +65,27 @@ public class ReportedMessageDTO extends EmailMessageDTO {
         this.sendingReport = sendingReport;
     }
 
-    /*
-    Retract access tokens from private/personal URLs in the message body so that these can be presented in the officer UI
-     */
+
     @Override
     public String getBody() {
-        List<String> regexps = new ArrayList<>();
-        regexps.add("(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]/(token|emailregister)/)[A-Za-z0-9]+");
+        return getFilteredBody();
+    }
 
-        String result = super.getBody();
+
+    /**
+     * Retract access tokens from private/personal URLs in the message body so that these can be presented in
+     * the officer UI
+     */
+    private String getFilteredBody() {
+        final List<String> regexps = Arrays.asList(
+                "(https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]/(token|emailregister)/)[A-Za-z0-9]+"
+        );
+        final String substitute = "$1[RETRACTED]";
+
+        String body  = super.getBody();
         for (String regexp : regexps) {
-            result = result.replaceAll(regexp, "$1[RETRACTED]");
+            body = body.replaceAll(regexp, substitute);
         }
-        return result;
+        return body;
     }
 }
