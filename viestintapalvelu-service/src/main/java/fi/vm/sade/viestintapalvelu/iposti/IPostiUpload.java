d96353f4b24c95a7907fd0fbbaecb78886a211ca
Jessika Penttinen <jessika.penttinen@siili.com>
2016-06-22 09:48:40 +0300

    Lisätty loggausta iPostin lähetykseen

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/iposti/IPostiUpload.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/iposti/IPostiUpload.java
@@ -17,16 +17,14 @@ package fi.vm.sade.viestintapalvelu.iposti;
 
 import java.io.ByteArrayInputStream;
 
+import com.jcraft.jsch.*;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-
 @Service
 public class IPostiUpload {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(IPostiUpload.class);
 
     @Value("${viestintapalvelu.iposti.username}")
     private String username;
@@ -41,20 +39,40 @@ public class IPostiUpload {
 
     public boolean upload(byte[] zipBytes, String filename) throws Exception {
         JSch jsch = new JSch();
-        Session session = jsch.getSession(username, hostname, Integer.parseInt(port));
-        session.setPassword(password);
-        java.util.Properties config = new java.util.Properties();
-        config.put("StrictHostKeyChecking", "no");
-        session.setConfig(config);
-        session.connect();
-        Channel channel = session.openChannel("sftp");
-        channel.connect();
-        ChannelSftp channelSftp = (ChannelSftp) channel;
-        channelSftp.cd(directory);
-        channelSftp.put(new ByteArrayInputStream(zipBytes), filename + ".temppi");
-        channelSftp.rename(filename + ".temppi", filename + ".ok");
-        channelSftp.exit();
-        session.disconnect();
+        Session session = null;
+        ChannelSftp channelSftp = null;
+
+        try {
+            LOGGER.info("Initializing session for SFTP connection for {} file upload...", filename);
+            session = jsch.getSession(username, hostname, Integer.parseInt(port));
+            session.setPassword(password);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+
+            LOGGER.info("Connecting to SFTP server for {} file upload...", filename);
+            session.connect();
+            Channel channel = session.openChannel("sftp");
+            channel.connect();
+            channelSftp = (ChannelSftp) channel;
+
+            LOGGER.info("Starting to upload file {} to SFTP server...", filename);
+            channelSftp.cd(directory);
+            channelSftp.put(new ByteArrayInputStream(zipBytes), filename + ".temppi");
+            channelSftp.rename(filename + ".temppi", filename + ".ok");
+            LOGGER.info("File upload {} is ready.", filename);
+
+        } catch(Exception e) {
+            LOGGER.error("Got error when uploading file " + filename + " to SFTP server!", e);
+            throw e;
+        } finally {
+            if(null != session) {
+                session.disconnect();
+            }
+            if(null != channelSftp) {
+                channelSftp.exit();
+            }
+        }
         return true;
     }
 
