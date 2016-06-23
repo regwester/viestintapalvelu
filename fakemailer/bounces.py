c3b93372edf26281aa907622077087052e7d2b30
Jussi Vesala <jussi.vesala@reaktor.fi>
2016-06-23 10:17:46 +0300

    create python script to parse bounce-csv files and combine them as common bounces.json

---
+++ fakemailer/bounces.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import json
+
+bounceFolder = "./bounces/"
+resultFile = "bounces.json"
+
+class BounceJsonCreator:
+    def create(self):
+        bounces = self.read_bounces_data()
+        json_data = { "bounced_emails" : self.convert(bounces) }
+        data = json.dumps(json_data, sort_keys=False, indent=2)
+        self.write_result(data)
+
+    def read_bounces_data(self):
+        bounces = []
+        for root, dirs, filenames in os.walk(bounceFolder):
+            for f in filenames:
+                bounceCsv = open(os.path.join(root, f),'r')
+                bounceData = bounceCsv.read().split(";")
+                bounces.append(bounceData)
+        return bounces
+
+    def convert(self, bounces):
+        return map(lambda x: { "email" : x[0], "bounce_cnt" : 1, "last_bounce" : x[1], "X-Batch-ID": "", "X-Message-ID": x[2]}, bounces)
+
+    def write_result(self, json):
+         file = open(bounceFolder + resultFile, "w")
+         file.write(json)
+         file.close
+
+if __name__ == "__main__":
+    creator = BounceJsonCreator()
+    creator.create()
