51c3068529ded0e76ab014dc2523d9bbb6175924
Jussi Vesala <jussi.vesala@reaktor.fi>
2016-08-05 10:14:31 +0300

    change transaction isolation level

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/letter/impl/LetterServiceImpl.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/letter/impl/LetterServiceImpl.java
@@ -46,6 +46,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -497,7 +498,7 @@ public class LetterServiceImpl implements LetterService {
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
     public void updateBatchProcessingStarted(long id, LetterBatchProcess process) {
         LetterBatch batch = letterBatchDAO.read(id);
         if (batch == null) {
