3968127f2f78783b8f9cf936bb23d08b949f9d9f
August Kilponen <august.kilponen@gmail.com>
2016-08-17 10:28:49 +0300

    Päivityksiä 2. asteen kirjeisiin

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -33,9 +33,11 @@ public class TemplateTestDataGenerator {
 
 
     public static void main(String[] args) throws IOException {
-        String templateKeys = System.getProperty("template");
+        //String templateKeys = System.getProperty("template");
         //String templateKeys = "2aste_jalkiohjauskirje_2016";
         //String templateKeys = "2aste_hyvaksymiskirje_2016,2aste_jalkiohjauskirje_2016";
+        //String templateKeys = "erkka_hyvaksymiskirje_2016,erkka_jalkiohjauskirje_2016";
+        String templateKeys = "nivel_hyvaksymiskirje_2016,nivel_jalkiohjauskirje_2016";
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
                 for (String language : asList("FI", "EN", "SV")) {
