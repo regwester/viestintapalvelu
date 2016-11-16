4d744ad28837ed9780940a69ba5836895bca149f
August Kilponen <august.kilponen@gmail.com>
2016-11-16 15:23:36 +0200

    Kk-kirjeiden muutoksia

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -37,7 +37,8 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "2aste_hyvaksymiskirje_2016,2aste_jalkiohjauskirje_2016";
         //String templateKeys = "erkka_hyvaksymiskirje_2016,erkka_jalkiohjauskirje_2016";
         //String templateKeys = "nivel_hyvaksymiskirje_2016,nivel_jalkiohjauskirje_2016";
-        String templateKeys = "2aste_lisahaku_hyvaksymiskirje_2016";
+        //String templateKeys = "2aste_lisahaku_hyvaksymiskirje_2016";
+        String templateKeys = "kk_hyvaksymiskirje_2016";
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
                 for (String language : asList("FI", "EN", "SV")) {
