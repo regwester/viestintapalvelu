1232ec9e44196f6fe094f34cb7d0cf3d13293c83
August Kilponen <august.kilponen@gmail.com>
2018-06-27 21:24:22 +0300

    Päivitetty kk 2018 kirjepohjia

--- viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
+++ viestintapalvelu-service/src/test/java/fi/vm/sade/viestintapalvelu/testdata/TemplateTestDataGenerator.java
@@ -42,7 +42,7 @@ public class TemplateTestDataGenerator {
         //String templateKeys = "amkope_hyvaksymiskirje_2017,amkope_jalkiohjauskirje_2017";
         //String templateKeys = "kk_varasijakirje_2016";
         //String templateKeys = "omattiedot_email";
-        String templateKeys = "erkka_hyvaksymiskirje_2018,erkka_jalkiohjauskirje_2018";
+        String templateKeys = "kk_hyvaksymiskirje_2018,kk_jalkiohjauskirje_2018";
         if(templateKeys != null) {
             for(String templateKey : templateKeys.split(",")) {
                 for (String language : asList("FI", "EN", "SV")) {
