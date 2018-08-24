499e7495863139b4a858e3ffb36fa542f01140cb
teo.mertanen <teo.mertanen@gofore.com>
2018-08-24 16:36:18 +0300

    BUG-1710 replace emailResourceClient with CachingRestClient implementation

--- viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/externalinterface/component/EmailComponent.java
+++ viestintapalvelu-service/src/main/java/fi/vm/sade/viestintapalvelu/externalinterface/component/EmailComponent.java
@@ -17,10 +17,8 @@ package fi.vm.sade.viestintapalvelu.externalinterface.component;
 
 import java.io.InputStream;
 
-import javax.annotation.Resource;
-import javax.ws.rs.core.Response;
-
 import fi.vm.sade.viestintapalvelu.common.exception.ExternalInterfaceException;
+import fi.vm.sade.viestintapalvelu.externalinterface.RyhmasahkopostiRestClient;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,20 +34,30 @@ import fi.vm.sade.viestintapalvelu.externalinterface.api.EmailResource;
 public class EmailComponent {
     private static Logger LOGGER = LoggerFactory.getLogger(EmailComponent.class);
 
-    @Resource
-    private EmailResource emailResourceClient;
+    private final EmailResource ryhmasahkopostiRestClient;
 
     @Autowired
     private EmailBuilder emailBuilder;
 
+    @Autowired
+    public EmailComponent(RyhmasahkopostiRestClient ryhmasahkopostiRestClient) {
+        this.ryhmasahkopostiRestClient = ryhmasahkopostiRestClient;
+    }
+
     public boolean sendEmail(EmailData data) {
-        return checkResponse(emailResourceClient.sendEmail(data));
+        try {
+            ryhmasahkopostiRestClient.sendEmail(data);
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Email sending request failed for data " + data + ", reason: " + e.getMessage(), e);
+            return false;
+        }
     }
 
     public String getPreview(EmailData data) {
         try {
             LOGGER.warn("Calling external interface EmailResource.getPreview");
-            InputStream stream = (InputStream) emailResourceClient.getPreview(data).getEntity();
+            InputStream stream = (InputStream) ryhmasahkopostiRestClient.getPreview(data).getEntity();
             return IOUtils.toString(stream);
         } catch (Exception e) {
             LOGGER.error("Could not make preview for email " + data + ". Reason: " + e.getMessage(), e);
@@ -67,11 +75,13 @@ public class EmailComponent {
             LOGGER.error("Could not make email data for letter " + source + " reason " + e.getMessage(), e);
             return false;
         }
-        return checkResponse(emailResourceClient.sendEmail(emailData));
-    }
 
-    private boolean checkResponse(Response response) {
-        LOGGER.debug("Got email response: " + response.toString() + " " + response.getStatus());
-        return true;
+        try {
+            ryhmasahkopostiRestClient.sendEmail(emailData);
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Email sending request failed for letter " + source + ", reason: " + e.getMessage(), e);
+            return false;
+        }
     }
 }
